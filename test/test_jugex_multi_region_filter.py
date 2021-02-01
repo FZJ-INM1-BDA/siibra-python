67e7099d930c70437bd0b030516656bc116d4052
Stefan Köhnen <s.koehnen@fz-juelich.de>
2021-02-01 12:03:28 +0100

    Add test for the filtering of duplicate samples

---
+++ test/test_jugex_multi_region_filter.py
@@ -0,0 +1,28 @@
+import unittest
+import brainscapes as bs
+
+class TestJugexMultiRegionFilter(unittest.TestCase):
+
+    def test_region_filter(self):
+        bs.logger.setLevel("INFO")
+        atlas = bs.atlases.MULTILEVEL_HUMAN_ATLAS
+        atlas.select_parcellation(bs.parcellations.JULICH_BRAIN_PROBABILISTIC_CYTOARCHITECTONIC_MAPS_V2_5_)
+        atlas.enable_continuous_map_thresholding(0.2)
+        jugex = bs.analysis.DifferentialGeneExpression(atlas)
+
+        input_dict = {
+            (1.1111, 2.222, 3.333, "region-name"): {},
+            (1.2345, 2.345, 3.000, "region-name"): {},
+            (1.1111, 2.222, 3.333, "another region-name"): {}
+        }
+
+        expected_dict = {
+            (1.1111, 2.222, 3.333, "region-name"): {},
+            (1.2345, 2.345, 3.000, "region-name"): {}
+        }
+
+        self.assertEqual(jugex._filter_samples(input_dict), expected_dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
