d8a1f5dab4bdd2e153d3c75ce81fd6da8564bbcc
Stefan Köhnen <s.koehnen@fz-juelich.de>
2021-02-05 13:41:01 +0100

    Add test-case for file retrieval

---
+++ test/test_retrieval_download_file.py
@@ -0,0 +1,14 @@
+import unittest
+import brainscapes as bs
+
+class TestRetrievalDownloadFile(unittest.TestCase):
+
+    def test_download_file(self):
+        bs.retrieval.download_file(
+            "https://object.cscs.ch/v1/AUTH_227176556f3c4bb38df9feea4b91200c/test_stefan_destination/MPM.zip",
+            ziptarget="JulichBrain_v25.xml"
+        )
+    # TODO Clear cache folder after test (for local testing)
+
+if __name__ == "__main__":
+    unittest.main()
