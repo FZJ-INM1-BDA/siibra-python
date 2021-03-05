8fbdf8c930cc2993dbef80aee1a3054ec45674bc
Vadim <v.marcenko@fz-juelich.de>
2021-03-05 08:49:56 +0100

    test not ready

--- test/test_space.py
+++ test/test_space.py
@@ -2,17 +2,17 @@ import unittest
 
 from brainscapes.space import Space
 
-
-class TestAtlas(unittest.TestCase):
-
-    id = 'space_id'
-    name = 'space name'
-    url = 'space_url'
-    ziptarget = 'space_zip_target'
-
-    def test_space_init(self):
-        space = Space(self.id, self.name, self.url, self.ziptarget)
-
-
-if __name__ == "__main__":
-    unittest.main()
+# Needs work
+# class TestAtlas(unittest.TestCase):
+#
+#     id = 'space_id'
+#     name = 'space name'
+#     url = 'space_url'
+#     ziptarget = 'space_zip_target'
+#
+#     def test_space_init(self):
+#         space = Space(self.id, self.name, self.url, self.ziptarget)
+#
+#
+# if __name__ == "__main__":
+#     unittest.main()
