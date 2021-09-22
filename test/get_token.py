f1fae3d98adfb649d5830594ad8b653a19bd6e22
Stefan Köhnen <s.koehnen@fz-juelich.de>
2021-09-22 12:02:33 +0200

    Add scope

--- test/get_token.py
+++ test/get_token.py
@@ -27,7 +27,7 @@ def _check_envs():
 
 
 def _build_request_object():
-    request_template = Template('grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}')
+    request_template = Template('grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&scope=kg-nexus-role-mapping,kg-nexus-service-account-mock')
     result = request_template.substitute(
                 CLIENT_ID = os.environ[CLIENT_ID_env],
                 CLIENT_SECRET = os.environ[CLIENT_SECRET_env]
