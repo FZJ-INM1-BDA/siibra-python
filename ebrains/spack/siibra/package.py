7b7b8c2beebe083266c6676cc3a4892a18de63fc
vadim <v.marcenko@fz-juelich.de>
2022-01-24 12:21:03 +0100

    changes for version hash after first tests with spack (at least no format errors)

--- ebrains/spack/siibra/package.py
+++ ebrains/spack/siibra/package.py
@@ -1,27 +1,22 @@
 from spack import *
 
-class Siibra(PythonPackage):
-    """
-    Software interfaces for interacting with brain atlases
-    """
 
-    homepage = 'https://readthedocs.org/projects/siibra-python/'
-    git = 'https://github.com/FZJ-INM1-BDA/siibra-python'
-    pypi = 'siibra/siibra-0.3a11.tar.gz'
+class Siibra(PythonPackage):
+    """Software interfaces for interacting with brain atlases."""
+
+    homepage = "https://readthedocs.org/projects/siibra-python/"
+    git = "https://github.com/FZJ-INM1-BDA/siibra-python"
+    pypi     = "siibra/siibra-0.3a11.tar.gz"
+
     maintainers = ['Timo Dickscheid', 'Xiao Gui', 'Vadim Marcenko']
 
-    version('0.3a11', '2e438db14f230cea99e290f36e9d0e61')
-    version('0.3a10', 'fa7fba0b491f4b8655e2a0c1902b78ba')
-    version('0.3a8', 'd9d7cf591c287ea09a562e9bf552506c')
-    version('0.3a6', '80f30eace3520e702a26f4a9d5747b59')
+    version('0.3a11', sha256='d4b6a1b9a49823403dc3d29b8a7aa38f2f832ec6a324d8ae39a68b082bdd50a6')
+    version('0.3a10', sha256='f2994cc60baa7a0a5b54ce5cead2439a444d975b0e1d13b1c83754d97e5fc4a3')
+    version('0.3a8', sha256='550658e16bc9ff169128217bcb7e3fc6e281f17dba11e168cf1cd756fdce298c')
 
-    # python_requires
     depends_on('python@3.6:', type=('build', 'run'))
-
-    # setup_requires
     depends_on('py-setuptools', type='build')
 
-    # install_requires
     depends_on('anytree', type=('build', 'run'))
     depends_on('nibabel', type=('build', 'run'))
     depends_on('click', type=('build', 'run'))
