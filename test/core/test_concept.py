e560db7ab710dbd958c0fc2cb9c0811e3277d372
vadim <v.marcenko@fz-juelich.de>
2022-03-11 11:53:39 +0100

    Add a test to check, that NewConnectionError is handled

--- test/core/test_concept.py
+++ test/core/test_concept.py
@@ -1,22 +1,36 @@
 from siibra.core import concept
 from unittest.mock import MagicMock
 from siibra.retrieval.exceptions import NoSiibraConfigMirrorsAvailableException
-
+from urllib3.exceptions import NewConnectionError
 
 import pytest
 
 
-def test_provide_registry_tries_mirrors():
-    
+class DummyCls:
+    _bootstrap_folder = "dummy_folder"
+    pass
+
+
+def test_number_of_connectors():
     assert len(concept._BOOTSTRAP_CONNECTORS) == 2, f"expect exactly 2 mirrors"
 
+
+def test_provide_registry_tries_mirrors():
     for connector in concept._BOOTSTRAP_CONNECTORS:
         connector.get_loaders = MagicMock()
         connector.get_loaders.side_effect = Exception("mock request exception")
-    
-    class DummyCls:
-        _bootstrap_folder = "dummy_folder"
-        pass
+
+    with pytest.raises(NoSiibraConfigMirrorsAvailableException):
+        concept.provide_registry(DummyCls)
+
+    for connector in concept._BOOTSTRAP_CONNECTORS:
+        connector.get_loaders.assert_called()
+
+
+def test_provide_registry_with_connection_error():
+
+    concept._BOOTSTRAP_CONNECTORS[0].get_loaders = MagicMock()
+    concept._BOOTSTRAP_CONNECTORS[0].get_loaders.side_effect = NewConnectionError(None, "mock request error")
 
     with pytest.raises(NoSiibraConfigMirrorsAvailableException):
         concept.provide_registry(DummyCls)
