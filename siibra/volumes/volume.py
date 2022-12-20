35dffcd88ff5b67bb3f45f70397208b536c426fe
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2022-12-20 16:36:41 +0100

    removed variant info from volume.fetch docstring as it is not taking variant anymore

--- siibra/volumes/volume.py
+++ siibra/volumes/volume.py
@@ -110,11 +110,6 @@ class Volume:
             - supported format types, see Volume.PREFERRED_FORMATS. This includes
               "nii", "zip/nii", "neuroglancer/precomputed", "gii-mesh", 
               "neuroglancer/precompmesh", "gii-label"
-        variant: str
-            Some volume providers allow acces to different variants, such as
-            freesurfer surface meshes which are available in the pial, white matter,
-            or inflated variant. If not used, multiple variants will be combined into
-            a multi-fragment mesh.
         """
         if voi and (voi.space != self.space):
             logger.info(f"Warping volume of interest from {voi.space.name} to {self.space.name}.")
