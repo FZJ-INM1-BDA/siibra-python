d3174a8b98069180a2a5608a44a8fde11625ed8b
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2023-01-19 17:19:49 +0100

    Temp fix to allen querries and its example

--- siibra/features/molecular/gene_expression.py
+++ siibra/features/molecular/gene_expression.py
@@ -95,7 +95,7 @@ class GeneExpressions(tabular.Tabular):
 
         data = pd.DataFrame(
             dict(
-                **{'level': levels, 'zscore': z_scores, 'gene': genes},
+                **{'level': levels, 'zscore': z_scores, 'gene': genes, 'anchor': anchor},
                 **additional_columns
             )
         )
