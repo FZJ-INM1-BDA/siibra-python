6cbd31db542f43b343823db96a16d77f92b9dd20
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2023-02-02 12:24:25 +0100

    Reenable logscale for connectivity heatmaps

--- siibra/features/basetypes/regional_connectivity.py
+++ siibra/features/basetypes/regional_connectivity.py
@@ -132,7 +132,7 @@ class RegionalConnectivity(Feature):
             self._matrices[subject] = self._load_matrix(subject)
         return self._matrices[subject]
 
-    def plot_matrix(self, subject: str = None, regions: List[str] = None, **kwargs):
+    def plot_matrix(self, subject: str = None, regions: List[str] = None, logscale: bool = False, **kwargs):
         """
         Plots the heatmap of the connectivity matrix using nilearn.plotting.
 
@@ -145,11 +145,15 @@ class RegionalConnectivity(Feature):
         regions: list[str]
             Display the matrix only for selected regions. By default, shows all the regions.
             It can only be a subset of regions of the feature.
+        logscale: bool
+            Display the data in log10 scale
         **kwargs:
             Can take all the arguments `nilearn.plotting.plot_matrix` can take. See the doc at
             https://nilearn.github.io/stable/modules/generated/nilearn.plotting.plot_matrix.html
         """
         matrix = self.get_matrix(subject=subject)
+        if logscale:
+            matrix = np.log10(matrix)
         if regions is None:
             regions = matrix.columns.to_list()
 
