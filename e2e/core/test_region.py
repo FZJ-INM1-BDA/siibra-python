d7a8dda6c6c8fbec153dd03bd3a4d42c044ce55b
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2023-04-20 15:33:53 +0200

    Add e2e for region.find

--- e2e/core/test_region.py
+++ e2e/core/test_region.py
@@ -1,5 +1,6 @@
 import pytest
 import siibra
+import re
 
 regions = [
     siibra.get_region("julich 3.0", "Area 4p (PreCG) right"),
@@ -20,8 +21,36 @@ regions_of_concern = [
 ]
 
 # Test duplicate named regions and regions with only child
-@pytest.mark.parametrize("parc_spec,region_name", regions_of_concern)
-def test_get_region(parc_spec,region_name):
+@pytest.mark.parametrize("parc_spec, region_name", regions_of_concern)
+def test_get_region(parc_spec, region_name):
     region = siibra.get_region(parc_spec, region_name)
     assert region
     assert region.name == region_name
+
+
+regionspecs = [
+    ("julich 3.0", "julich 3.0", 1,
+        [siibra.parcellations["julich 3.0"].name]
+    ),
+    ("waxholm 4", "lateral olfactory tract", 2,
+        ["lateral olfactory tract", "Nucleus of the lateral olfactory tract"]
+    ),
+    ("julich 2.9", "/area 4/i", 12,
+        ["Area 44 (IFG)", "Area 4p (PreCG) right", "Area 4a (PreCG) left"]
+    ),
+    ("Superficial fiber Bundles HCP", re.compile("rh_SF-SF_*"), 24,
+        ["rh_SF-SF_9", "rh_SF-SF_19", "rh_SF-SF_23"]
+    ),
+    ("julich 3.0", siibra.get_region("julich 3.0", "hoc1 left"), 1,
+        ["Area hOc1 (V1, 17, CalcS) left"]
+    )
+]
+@pytest.mark.parametrize(
+    "parc_spec, region_spec, result_len, check_regions", regionspecs
+)
+def test_get_region(parc_spec, region_spec, result_len, check_regions):
+    parc = siibra.parcellations.get(parc_spec)
+    results = parc.find(region_spec)
+    assert isinstance(results, list)
+    assert len(results) == result_len
+    assert all(r in results for r in check_regions)
