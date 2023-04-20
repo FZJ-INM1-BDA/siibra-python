3fddf55a330dddaa2a66c01ac051e3ba3977aaab
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2023-04-20 14:07:58 +0200

    Add e2e for duplicate named regions

--- e2e/core/test_region.py
+++ e2e/core/test_region.py
@@ -11,4 +11,16 @@ def test_region_spatial_props(region: siibra.core.parcellation.region.Region):
     props = region.spatial_props("mni152")
     for idx, cmp in enumerate(props.components, start=1):
         assert cmp.volume >= props.components[idx - 1].volume
-        
+
+
+regions_of_concern = [
+    ("julich 3.0", "frontal lobe"),
+    ("julich 1.18", "Ch 123 (Basal Forebrain) left"),
+    ("waxholm 4", "lateral olfactory tract")
+]
+
+# Test duplicate named regions and regions with only child
+@pytest.mark.parametrize("par_region_tuple", regions_of_concern)
+def test_get_region(par_region_tuple):
+    region = siibra.get_region(par_region_tuple[0], par_region_tuple[1])
+    assert region.name == par_region_tuple[1]
