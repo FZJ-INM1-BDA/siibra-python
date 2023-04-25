f2c63076726122335389dafa48de58e8d361cdd8
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2023-04-25 14:58:54 +0200

    Use unique name for the cache of sparseindices

--- siibra/volumes/sparsemap.py
+++ siibra/volumes/sparsemap.py
@@ -245,7 +245,7 @@ class SparseMap(parcellationmap.Map):
     @property
     def sparse_index(self):
         if self._sparse_index_cached is None:
-            prefix = f"{self.parcellation.id}_{self.space.id}_{self.maptype}_index"
+            prefix = f"{self.name}_index"
             spind = SparseIndex.from_cache(prefix)
             with _volume.SubvolumeProvider.UseCaching():
                 if spind is None:
