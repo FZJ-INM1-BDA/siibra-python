51aaf58466362ac3389ef322ab4fc41c269882e2
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2023-06-22 10:47:58 +0200

    bigbrain left and rigth calculation correction

--- siibra/livequeries/bigbrain.py
+++ siibra/livequeries/bigbrain.py
@@ -32,7 +32,8 @@ class WagstylProfileLoader:
     BRANCH = "main"
     PROFILES_FILE_LEFT = "https://data-proxy.ebrains.eu/api/v1/public/buckets/d-26d25994-634c-40af-b88f-2a36e8e1d508/profiles/profiles_left.txt"
     PROFILES_FILE_RIGHT = "https://data-proxy.ebrains.eu/api/v1/public/buckets/d-26d25994-634c-40af-b88f-2a36e8e1d508/profiles/profiles_right.txt"
-    THICKNESSES_FILE = "data/thicknesses_left.npy"
+    THICKNESSES_FILE_LEFT = "data/thicknesses_left.npy"
+    THICKNESSES_FILE_RIGHT = ""
     MESH_FILE_LEFT = "gray_left_327680.surf.gii"
     MESH_FILE_RIGHT = "gray_right_327680.surf.gii"
     BASEURL = "https://ftp.bigbrainproject.org/bigbrain-ftp/BigBrainRelease.2015/3D_Surfaces/Apr7_2016/gii/"
@@ -51,17 +52,17 @@ class WagstylProfileLoader:
     @classmethod
     def _load(cls):
         # read thicknesses, in mm, and normalize by their last column which is the total thickness
-        thickness_left = requests.HttpRequest(f"{cls.REPO}/raw/{cls.BRANCH}/{cls.THICKNESSES_FILE}").data.T
+        thickness_left = requests.HttpRequest(f"{cls.REPO}/raw/{cls.BRANCH}/{cls.THICKNESSES_FILE_LEFT}").data.T
         thickness_right = np.zeros(shape=thickness_left.shape)  # TODO: replace with thickness data for te right hemisphere
-        thickness = np.concatenate((thickness_left, thickness_right))
-        total_thickness = thickness[:, :-1].sum(1)
+        thickness = np.concatenate((thickness_left[:, :-1], thickness_right[:, :-1]))  # last column is the computed total thickness
+        total_thickness = thickness.sum(1)
         valid = np.where(total_thickness > 0)[0]
-        cls._boundary_depths = np.c_[np.zeros_like(valid), (thickness[valid, :-1] / total_thickness[valid, None]).cumsum(1)]
+        cls._boundary_depths = np.c_[np.zeros_like(valid), (thickness[valid, :] / total_thickness[valid, None]).cumsum(1)]
         cls._boundary_depths[:, -1] = 1  # account for float calculation errors
 
         # read profiles with valid thickenss
-        profile_left_url = f"{cls.PROFILES_FILE_LEFT}"
-        profile_right_url = f"{cls.PROFILES_FILE_RIGHT}"
+        profile_left_url = cls.PROFILES_FILE_LEFT
+        profile_right_url = cls.PROFILES_FILE_RIGHT
         if not all(
             path.exists(cache.CACHE.build_filename(url))
             for url in [profile_left_url, profile_right_url]
