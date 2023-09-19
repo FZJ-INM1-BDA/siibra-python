bde6c7bfd173b545bf197704bec4c9d6822f5bc8
AhmetNSimsek <ahmet.n.simsek@gmail.com>
2023-09-19 17:44:30 +0200

    Maint: Feature query
    - Update messages
    - Hide `parse_featuretype`
    - `parse_featuretype` excludes intermediary classes like RegionalConnecitivty to avoid repeated queries.
    - if string provided, query with the resulting feature type lists instead of set comprehension

--- siibra/features/feature.py
+++ siibra/features/feature.py
@@ -362,13 +362,17 @@ class Feature:
         return (F, concept, fid)
 
     @classmethod
-    def parse_featuretype(cls, feature_type: str) -> List[Type['Feature']]:
-        return [
+    def _parse_featuretype(cls, feature_type: str) -> List[Type['Feature']]:
+        ftypes = {
             feattype
             for FeatCls, feattypes in cls.SUBCLASSES.items()
             if all(w.lower() in FeatCls.__name__.lower() for w in feature_type.split())
             for feattype in feattypes
-        ]
+        }
+        if len(ftypes) > 1:
+            return [ft for ft in ftypes if getattr(ft, 'category')]
+        else:
+            return list(ftypes)
 
     @classmethod
     def livequery(cls, concept: Union[region.Region, parcellation.Parcellation, space.Space], **kwargs) -> List['Feature']:
@@ -392,7 +396,12 @@ class Feature:
         return live_instances
 
     @classmethod
-    def match(cls, concept: Union[region.Region, parcellation.Parcellation, space.Space], feature_type: Union[str, Type['Feature'], list], **kwargs) -> List['Feature']:
+    def match(
+        cls,
+        concept: Union[region.Region, parcellation.Parcellation, space.Space],
+        feature_type: Union[str, Type['Feature'], list],
+        **kwargs
+    ) -> List['Feature']:
         """
         Retrieve data features of the desired modality.
 
@@ -405,18 +414,28 @@ class Feature:
         """
         if isinstance(feature_type, list):
             # a list of feature types is given, collect match results on those
-            assert all((isinstance(t, str) or issubclass(t, cls)) for t in feature_type)
-            return list(set(sum((cls.match(concept, t, **kwargs) for t in feature_type), [])))
+            assert all(
+                (isinstance(t, str) or issubclass(t, cls))
+                for t in feature_type
+            )
+            return list(set(
+                sum((
+                    cls.match(concept, t, **kwargs) for t in feature_type
+                ), [])
+            ))
 
         if isinstance(feature_type, str):
             # feature type given as a string. Decode the corresponding class.
-            # Some string inputs, such as connectivity, may hit multiple matches
-            # In this case
-            candidates = cls.parse_featuretype(feature_type)
-            if len(candidates) == 0:
+            # Some string inputs, such as connectivity, may hit multiple matches.
+            ftype_candidates = cls._parse_featuretype(feature_type)
+            if len(ftype_candidates) == 0:
                 raise ValueError(f"feature_type {str(feature_type)} did not match with any features. Available features are: {', '.join(cls.SUBCLASSES.keys())}")
 
-            return list({feat for c in candidates for feat in cls.match(concept, c, **kwargs)})
+            logger.info(
+                f"'{feature_type}' decoded as feature type/s: "
+                f"{[c.__name__ for c in ftype_candidates]}."
+            )
+            return cls.match(concept, ftype_candidates, **kwargs)
 
         assert issubclass(feature_type, Feature)
 
