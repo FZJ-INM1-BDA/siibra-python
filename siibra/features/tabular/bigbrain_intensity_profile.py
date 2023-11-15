90aca90dc2b61eef52738d47c68fd227ae93d351
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2023-11-15 15:33:22 +0100

    BigBrainIntesnsityProfiles use data stored in https://github.com/kwagstyl/cortical_layers_tutorial. Clearly state this in the description.

--- siibra/features/tabular/bigbrain_intensity_profile.py
+++ siibra/features/tabular/bigbrain_intensity_profile.py
@@ -29,8 +29,9 @@ class BigBrainIntensityProfile(
         "cortical layers: Cortical and laminar thickness gradients diverge in sensory and "
         "motor cortices. PLoS Biology, 18(4), e3000678. "
         "http://dx.doi.org/10.1371/journal.pbio.3000678'."
-        "Taken from the tutorial at https://github.com/kwagstyl/cortical_layers_tutorial "
-        "and assigned to cytoarchitectonic regions of Julich-Brain."
+        "The data is taken from the tutorial at "
+        "https://github.com/kwagstyl/cortical_layers_tutorial. Each vertex is "
+        "assigned to the regional map when queried."
     )
 
     def __init__(
