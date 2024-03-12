ee6b554497b5b591c9fa58a0ea00148438c0cc2a
Timo Dickscheid <t.dickscheid@fz-juelich.de>
2024-03-12 12:33:07 +0100

    plane patch extraction returns Patch, Patch provides flip()

--- siibra/experimental/cortical_profile_sampler.py
+++ siibra/experimental/cortical_profile_sampler.py
@@ -21,40 +21,37 @@ import numpy as np
 
 
 class CorticalProfileSampler:
-    """ Samples cortical profiles from the cortical layer maps. """
+    """Samples cortical profiles from the cortical layer maps."""
 
     def __init__(self):
-        self.layermap = (
-            parcellation.Parcellation
-            .get_instance('cortical layers')
-            .get_map(space='bigbrain', maptype='labelled')
-        )
-        
+        self.layermap = parcellation.Parcellation.get_instance(
+            "cortical layers"
+        ).get_map(space="bigbrain", maptype="labelled")
+
     def query(self, query_point: point.Point):
         q = query_point.warp(self.layermap.space)
         smallest_dist = np.inf
         best_match = None
         for layername in self.layermap.regions:
-            vertices = self.layermap.fetch(region=layername, format='mesh')["verts"]
-            dists = np.sqrt(((vertices - q.coordinate)**2).sum(1))
+            vertices = self.layermap.fetch(region=layername, format="mesh")["verts"]
+            dists = np.sqrt(((vertices - q.coordinate) ** 2).sum(1))
             best = np.argmin(dists)
             if dists[best] < smallest_dist:
                 best_match = (layername, best)
                 smallest_dist = dists[best]
 
         best_vertex = best_match[1]
-        hemisphere = 'left' if 'left' in best_match[0] else 'right'
+        hemisphere = "left" if "left" in best_match[0] else "right"
         print(f"Best match is vertex #{best_match[1]} in {best_match[0]}.")
 
         profile = [
-            (_, self.layermap.fetch(region=_, format='mesh')["verts"][best_vertex])
-            for _ in self.layermap.regions 
+            (_, self.layermap.fetch(region=_, format="mesh")["verts"][best_vertex])
+            for _ in self.layermap.regions
             if hemisphere in _
         ]
-   
+
         return contour.Contour(
             [p[1] for p in profile],
             space=self.layermap.space,
-            labels=[p[0] for p in profile]
+            labels=[p[0] for p in profile],
         )
-
--- siibra/experimental/plane3d.py
+++ siibra/experimental/plane3d.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 from . import contour
+from . import patch
 from ..locations import point, pointset
 from ..volumes import volume
 
@@ -21,50 +22,50 @@ import numpy as np
 
 
 class Plane3D:
-    """ 
+    """
     A 3D plane in reference space.
     This shall eventually be derived from siibra.Location
     """
-    
+
     def __init__(self, point1: point.Point, point2: point.Point, point3: point.Point):
-        """ 
-        Create a 3D plane from 3 points. 
-        The plane's reference space is defined by the first point. 
+        """
+        Create a 3D plane from 3 points.
+        The plane's reference space is defined by the first point.
         """
         self.space = point1.space
         # normal is the cross product of two arbitray in-plane vectors
         n = np.cross(
             (point2.warp(self.space) - point1).coordinate,
-            (point3.warp(self.space) - point1).coordinate
+            (point3.warp(self.space) - point1).coordinate,
         )
         self._n = n / np.linalg.norm(n)
         # distance from origin is the projection of any in-plane point onto the normal
         self._d = np.dot(point1.coordinate, self._n)
-    
+
     @property
     def normal(self):
         return self._n
-    
+
     @property
     def distance_from_origin(self):
         return self._d
-    
+
     def sidedness(self, points: np.ndarray):
-        """ 
-        Computes labels for a set of 3D coordinates classifying them 
+        """
+        Computes labels for a set of 3D coordinates classifying them
         by the halfspaces spanned by this plane.
         """
         assert points.shape[1] == 3
-        return (np.dot(points, self.normal) >= self.distance_from_origin).astype('int')
-    
+        return (np.dot(points, self.normal) >= self.distance_from_origin).astype("int")
+
     def intersect_line_segments(self, startpoints: np.ndarray, endpoints: np.ndarray):
         """
         Intersects a set of straight line segments with the plane.
         Returns the set of intersection points.
         The line segments are given by two Nx3 arrays of their start- and endpoints.
         The result is an Nx3 list of intersection coordinates.
-        TODO This returns an intersection even if the line segment intersects the plane, 
-        
+        TODO This returns an intersection even if the line segment intersects the plane,
+
         """
         directions = endpoints - startpoints
         lengths = np.linalg.norm(directions, axis=1)
@@ -83,33 +84,37 @@ class Plane3D:
         return result
 
     def intersect_mesh(self, mesh: dict):
-        """ 
+        """
         Intersects a 3D surface mesh with the plane.
         Returns a set of split 2D contours, represented by ordered coordinate lists.
         The output contour coordinates are intersection points of mesh edges going through the plane,
         and only combined into a contour if arising from neighboring edges in the mesh.
-        The mesh is passed as a dictionary with an Nx3 array "verts" of vertex coordinates, 
+        The mesh is passed as a dictionary with an Nx3 array "verts" of vertex coordinates,
         and an Mx3 array "faces" of face definitions.
         Each row in the face array corresponds to the three indices of vertices making up the
         triangle.
-        The result is a list of contour segments, each represented as a PointSet 
+        The result is a list of contour segments, each represented as a PointSet
         holding the ordered list of contour points.
-        The point labels in each "contour" PointSet hold the index of the face in the 
+        The point labels in each "contour" PointSet hold the index of the face in the
         mesh which made up each contour point.
         """
-        
+
         # select faces whose vertices are in different halfspaces relative to the y plane
         vertex_in_halfspace = self.sidedness(mesh["verts"])
         face_vertex_in_halfspace = vertex_in_halfspace[mesh["faces"]]
-        face_indices = np.where(face_vertex_in_halfspace.min(1) != face_vertex_in_halfspace.max(1))[0]
+        face_indices = np.where(
+            face_vertex_in_halfspace.min(1) != face_vertex_in_halfspace.max(1)
+        )[0]
         faces = mesh["faces"][face_indices]
 
-        # for each of N selected faces, indicate wether we cross the plane 
+        # for each of N selected faces, indicate wether we cross the plane
         # as we go from vertex 2->0, 0->1, 1->2, respectively.
-        # This gives us an Nx3 array, where forward crossings are identified by 1, 
+        # This gives us an Nx3 array, where forward crossings are identified by 1,
         # and backward crossings by -1.
-        # Each column of the crossings is linked to two columns of the faces array. 
-        crossings = np.diff(face_vertex_in_halfspace[face_indices][:, [2, 0, 1, 2]], axis=1)
+        # Each column of the crossings is linked to two columns of the faces array.
+        crossings = np.diff(
+            face_vertex_in_halfspace[face_indices][:, [2, 0, 1, 2]], axis=1
+        )
         face_columns = np.array([[2, 0], [0, 1], [1, 2]])
 
         # We assume that there is exactly one forward and one inverse crossing
@@ -118,33 +123,35 @@ class Plane3D:
         assert all(all((crossings == v).sum(1) == 1) for v in [-1, 0, 1])
 
         # Compute the actual intersection points for forward and backward crossing edges.
-        fwd_columns = np.where(crossings ==  1)[1]
+        fwd_columns = np.where(crossings == 1)[1]
         bwd_columns = np.where(crossings == -1)[1]
-        fwd_vertices = np.array([
-            faces[np.arange(len(faces)), face_columns[fwd_columns][:, 0]],
-            faces[np.arange(len(faces)), face_columns[fwd_columns][:, 1]]
-        ]).T
-        bwd_vertices = np.array([
-            faces[np.arange(len(faces)), face_columns[bwd_columns][:, 1]],
-            faces[np.arange(len(faces)), face_columns[bwd_columns][:, 0]]
-        ]).T
+        fwd_vertices = np.array(
+            [
+                faces[np.arange(len(faces)), face_columns[fwd_columns][:, 0]],
+                faces[np.arange(len(faces)), face_columns[fwd_columns][:, 1]],
+            ]
+        ).T
+        bwd_vertices = np.array(
+            [
+                faces[np.arange(len(faces)), face_columns[bwd_columns][:, 1]],
+                faces[np.arange(len(faces)), face_columns[bwd_columns][:, 0]],
+            ]
+        ).T
         fwd_intersections = self.intersect_line_segments(
-            mesh["verts"][fwd_vertices[:, 0]], 
-            mesh["verts"][fwd_vertices[:, 1]]
+            mesh["verts"][fwd_vertices[:, 0]], mesh["verts"][fwd_vertices[:, 1]]
         )
         bwd_intersections = self.intersect_line_segments(
-            mesh["verts"][bwd_vertices[:, 0]], 
-            mesh["verts"][bwd_vertices[:, 1]]
+            mesh["verts"][bwd_vertices[:, 0]], mesh["verts"][bwd_vertices[:, 1]]
         )
 
         # By construction, the fwd and backward intersections
         # should include the exact same set of points. Verify this now.
         sortrows = lambda A: A[np.lexsort(A.T[::-1]), :]
         err = (sortrows(fwd_intersections) - sortrows(bwd_intersections)).sum()
-        assert err == 0    
+        assert err == 0
 
-        # Due to the above property, we can construct closed contours in the 
-        # intersection plane by following the interleaved fwd/bwd roles of intersection 
+        # Due to the above property, we can construct closed contours in the
+        # intersection plane by following the interleaved fwd/bwd roles of intersection
         # points.
         face_indices = list(range(fwd_intersections.shape[0]))
         result = []
@@ -157,14 +164,12 @@ class Plane3D:
             p = fwd_intersections[face_id]
             points.append(p)
             # Remember the ids of the face and start-/end vertices for the point
-            labels.append(
-                (face_id, fwd_vertices[face_id, 0], fwd_vertices[face_id, 1])
-            )
+            labels.append((face_id, fwd_vertices[face_id, 0], fwd_vertices[face_id, 1]))
             face_indices.remove(face_id)
             neighbours = np.where(np.all(np.isin(bwd_intersections, p), axis=1))[0]
-            assert len(neighbours) == 1
+            assert len(neighbours) > 0
             face_id = neighbours[0]
-            if face_id in face_indices:  
+            if face_id in face_indices:
                 # more points available in the contour
                 continue
 
@@ -172,7 +177,7 @@ class Plane3D:
             result.append(
                 contour.Contour(np.array(points), labels=labels, space=self.space)
             )
-            if len(face_indices) > 0: 
+            if len(face_indices) > 0:
                 # prepare to process another contour segment
                 face_id = face_indices[0]
                 points = []
@@ -181,72 +186,69 @@ class Plane3D:
         return result
 
     def project_points(self, points: pointset.PointSet):
-        """ projects the given points onto the plane. """
+        """projects the given points onto the plane."""
         assert self.space == points.space
         XYZ = points.coordinates
         N = XYZ.shape[0]
         dists = np.dot(self._n, XYZ.T) - self._d
         return pointset.PointSet(
-            XYZ - np.tile(self._n, (N, 1)) * dists[:, np.newaxis],
-            space = self.space
+            XYZ - np.tile(self._n, (N, 1)) * dists[:, np.newaxis], space=self.space
         )
 
     def get_enclosing_patch(self, points: pointset.PointSet, margin=[0, 0]):
         """
         Computes the enclosing patch in the given plane
-        which contains the projections of the given points. 
+        which contains the projections of the given points.
         The orientation of the patch follows the principal axis.
         """
         projections = self.project_points(points)
-        
-        # compute PCA of point projections to obtain two orthogonal 
+
+        # compute PCA of point projections to obtain two orthogonal
         # in-plane spanning vectors
         XYZ = np.copy(projections.coordinates)
         mu = XYZ.mean(0)
         XYZ -= mu
         cov = np.dot(XYZ.T, XYZ)
-        eigvals_ , eigvecs_ = np.linalg.eigh(cov)
+        eigvals_, eigvecs_ = np.linalg.eigh(cov)
         eigvecs = eigvecs_[:, ::-1].T
         v1, v2 = [-eigvecs[_] for _ in np.argsort(eigvals_)[:2]]
-        
+
         # get extremal points along first spanning vector
         order = np.argsort(np.dot(projections.coordinates, v1))
-        p0 = projections[order[ 0]].homogeneous[0, :3]
+        p0 = projections[order[0]].homogeneous[0, :3]
         p1 = projections[order[-1]].homogeneous[0, :3]
 
         # get extremal points along second spanning vector
         order = np.argsort(np.dot(projections.coordinates, v2))
-        p2 = projections[order[ 0]].homogeneous[0, :3]
+        p2 = projections[order[0]].homogeneous[0, :3]
         p3 = projections[order[-1]].homogeneous[0, :3]
-        
+
         m0, m1 = margin
         w = np.linalg.norm(p3 - p2)
-        result = pointset.PointSet(
+        corners = pointset.PointSet(
             [
-                p1 + (w/2 + m1) * v2 + m0 * v1,
-                p0 + (w/2 + m1) * v2 - m0 * v1,
-                p0 - (w/2 + m1) * v2 - m0 * v1,
-                p1 - (w/2 + m1) * v2 + m0 * v1,
+                p1 + (w / 2 + m1) * v2 + m0 * v1,
+                p0 + (w / 2 + m1) * v2 - m0 * v1,
+                p0 - (w / 2 + m1) * v2 - m0 * v1,
+                p1 - (w / 2 + m1) * v2 + m0 * v1,
             ],
-            space = self.space
+            space=self.space,
         )
-        assert np.all(self.project_points(result).coordinates == result.coordinates)
-        return result
+        assert np.all(self.project_points(corners).coordinates == corners.coordinates)
+        return patch.Patch(corners)
 
     @classmethod
     def from_image(cls, image: volume.Volume):
-        """ 
+        """
         Derive an image plane by assuming the volume to be a 2D image.
         The smallest dimension in voxel space is considered flat.
-        The plane is defined in the physical space of the volume. 
+        The plane is defined in the physical space of the volume.
         """
         assert isinstance(image, volume.Volume)
         im_lowres = image.fetch(resolution_mm=1)
         plane_dims = np.where(np.argsort(im_lowres.shape) < 2)[0]
         voxels = pointset.PointSet(
-            np.vstack(([0, 0, 0], np.identity(3)[plane_dims])),
-            space=None
+            np.vstack(([0, 0, 0], np.identity(3)[plane_dims])), space=None
         )
         points = voxels.transform(im_lowres.affine, space=image.space)
         return cls(points[0], points[1], points[2])
-
