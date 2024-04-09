040f14471ba2e01d8f4965c9ca9c8e8a1f75a894
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-04-09 10:07:24 +0200

    Revert "Use config: add_visfAtlas for tests"
    This reverts commit d8de38a4c5a736fe50dae706ae0edb763d054af6.

--- siibra/configuration/configuration.py
+++ siibra/configuration/configuration.py
@@ -42,7 +42,7 @@ class Configuration:
         conn(
             server_or_owner,
             project_or_repo,
-            reftag="add_visfAtlas",
+            reftag="siibra-{}".format(__version__),
             skip_branchtest=True
         )
         for conn, server_or_owner, project_or_repo in CONFIG_REPOS
