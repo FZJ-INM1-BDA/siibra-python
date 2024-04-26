290096c6f7ea2d7ac9f67e0bd61bbf90d9049997
Ahmet Nihat Simsek <69900321+AhmetNSimsek@users.noreply.github.com>
2024-04-26 13:58:57 +0200

    Create docs when pushed to main or braches starting w "doc_". Run examples with e2e test suite and choose python version accordingly

--- e2e/test_examples.py
+++ e2e/test_examples.py
@@ -7,10 +7,7 @@ examples = pathlib.Path('./examples').resolve().rglob('*.py')
 python_version = sys.version_info
 
 
-@pytest.mark.skipif(
-    (python_version.major, python_version.minor) == (3, 7),
-    reason="Examples are expensive, only test with the oldest suported version."
-)
 @pytest.mark.parametrize('example', examples)
 def test_script_execution(example):
+    print("Running:", example)
     runpy.run_path(example)
