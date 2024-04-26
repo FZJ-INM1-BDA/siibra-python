1d1e3517eb47069889ad3b71c1efe36fc835d1f4
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-04-26 15:39:33 +0200

    Build core of beta feature module

--- siibra/features_beta/attributes/base.py
+++ siibra/features_beta/attributes/base.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass, field
 
+
 @dataclass
 class Attribute:
     type: str = field(default="attr", init=False)
     key: str = field(default=None, init=False)
     value: str = field(default=None, init=False)
-
-    def filter(self, *args, **kwargs):
-        return True
