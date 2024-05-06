230a34b82b32b24dad95b4c94ea1531684442e93
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-05-06 13:16:17 +0200

    Make dataclasses <3.10 compatible and fix lint

--- siibra/features_beta/attributes/data_attributes.py
+++ siibra/features_beta/attributes/data_attributes.py
@@ -7,12 +7,14 @@ from concurrent.futures import ThreadPoolExecutor
 from .base import Attribute
 from ...commons import logger
 from ...locations import BoundingBox, Location
+
 if TYPE_CHECKING:
     from ...volumes import volume
     from .meta_attributes import PolylineDataAttribute
 from ...retrieval import CACHE
 
 _m = Memory(CACHE.folder, verbose=False)
+
 
 @dataclass
 class DataAttribute(Attribute):
@@ -30,6 +32,7 @@ class DataAttribute(Attribute):
 
 BIGBRAIN_VOLUMETRIC_SHRINKAGE_FACTOR = 1.931
 
+
 @dataclass
 class TabularDataAttribute(DataAttribute):
 
@@ -38,12 +41,12 @@ class TabularDataAttribute(DataAttribute):
     url: str = None
     plot_options: dict = field(default_factory=dict)
     parse_options: dict = field(default_factory=dict)
-    
+
     @staticmethod
     @_m.cache
     def _GetData(url: str, parse_options: dict):
         return pd.read_csv(url, **parse_options)
-        
+
     @property
     def data(self) -> pd.DataFrame:
         return TabularDataAttribute._GetData(self.url, self.parse_options)
@@ -54,12 +57,11 @@ class TabularDataAttribute(DataAttribute):
         return self.data.plot(*args, **plot_kwargs)
 
 
-
 @dataclass
 class LayerBoundaryDataAttribute(DataAttribute):
     schema: str = "siibra/attr/data/layerboundary"
     url: str = None
-    
+
     @staticmethod
     @_m.cache
     def _GetPolylineAttributes(url: str):
@@ -70,8 +72,11 @@ class LayerBoundaryDataAttribute(DataAttribute):
 
         LAYERS = ("0", "I", "II", "III", "IV", "V", "VI", "WM")
 
-        all_betweeners = ("0" if start == "0" else f"{start}_{end}" for start,end in zip(LAYERS[:-1], LAYERS[1:]))
-        
+        all_betweeners = (
+            "0" if start == "0" else f"{start}_{end}"
+            for start, end in zip(LAYERS[:-1], LAYERS[1:])
+        )
+
         def return_segments(url):
             resp = requests.get(url)
             resp.raise_for_status()
@@ -82,17 +87,18 @@ class LayerBoundaryDataAttribute(DataAttribute):
 
         with ThreadPoolExecutor() as ex:
             segments = ex.map(
-                return_segments,
-                (f"{url}{p}.json" for p in all_betweeners)
+                return_segments, (f"{url}{p}.json" for p in all_betweeners)
             )
-        
-        return [PolylineDataAttribute(closed=False,
-                                      coordinates=poly_srt(np.array(s)).tolist()
-                                      ) for s in segments]
-        
+
+        return [
+            PolylineDataAttribute(
+                closed=False, coordinates=poly_srt(np.array(s)).tolist()
+            )
+            for s in segments
+        ]
 
     @property
-    def layers(self) -> list['PolylineDataAttribute']:
+    def layers(self) -> list["PolylineDataAttribute"]:
         return LayerBoundaryDataAttribute._GetPolylineAttributes(self.url)
 
 
@@ -105,11 +111,14 @@ class VoiDataAttribute(DataAttribute):
 
     @staticmethod
     @_m.cache
-    def _GetBoundingBox(providers: dict[str, str], clip: bool=False):
+    def _GetBoundingBox(providers: dict[str, str], clip: bool = False):
         from ...volumes.providers import VolumeProvider
+
         for provider in providers:
             try:
-                volume_provider = VolumeProvider._SUBCLASSES[provider](url=providers[provider])
+                volume_provider = VolumeProvider._SUBCLASSES[provider](
+                    url=providers[provider]
+                )
                 bbox = volume_provider.get_boundingbox(clip)
                 return (
                     [p for p in bbox.minpoint],
@@ -117,30 +126,35 @@ class VoiDataAttribute(DataAttribute):
                     bbox.space and bbox.space.id,
                 )
             except Exception as e:
-                logger.warn(f"Failed to get boundingbox for {provider} with url {providers[provider]}: {str(e)}")
+                logger.warn(
+                    f"Failed to get boundingbox for {provider} with url {providers[provider]}: {str(e)}"
+                )
         raise RuntimeError
 
     @property
     def boundingbox(self) -> "BoundingBox":
         minp, maxp, space_id = VoiDataAttribute._GetBoundingBox(self.providers)
         from ...locations import BoundingBox
+
         return BoundingBox(minp, maxp, space_id or self.space_id)
 
     @property
     def data(self) -> "volume.Volume":
         from ...volumes import volume, providers
-        
+
         return volume.Volume(
             space_spec={"@id": self.space_id},
             providers={
-                provider: providers.VolumeProvider._SUBCLASSES[provider](url=self.providers[provider])
+                provider: providers.VolumeProvider._SUBCLASSES[provider](
+                    url=self.providers[provider]
+                )
                 for provider in self.providers
             },
         )
 
     def plot(self, *args, **kwargs):
         raise NotImplementedError
-    
+
     def matches(self, first_arg=None, *args, **kwargs):
         if isinstance(first_arg, Location) and first_arg.intersects(self.boundingbox):
             return True
--- siibra/features_beta/modality.py
+++ siibra/features_beta/modality.py
@@ -2,13 +2,16 @@ from ..commons import QUIET, KeyAccessor
 
 _accessor: KeyAccessor = None
 
+
 def __dir__():
     with QUIET:
         from .attributes.meta_attributes import ModalityAttribute
+
         modalities = ModalityAttribute._GetAll()
         global _accessor
         _accessor = KeyAccessor(names=list(modalities))
         return _accessor.__dir__()
+
 
 def __getattr__(key: str):
     if _accessor is None:
