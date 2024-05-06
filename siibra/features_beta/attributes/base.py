748409b8e16960a7f3286ebc2e5193621c27d356
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-05-06 13:16:17 +0200

    Make dataclasses <3.10 compatible and fix lint

--- siibra/features_beta/attributes/base.py
+++ siibra/features_beta/attributes/base.py
@@ -5,23 +5,26 @@ from ...commons import logger
 SCHEMAS = {}
 
 
-@dataclass(kw_only=True)
+@dataclass
 class Attribute:
-    """ Base clase for attributes. """
+    """Base clase for attributes."""
+
     schema: str = field(default="siibra/attr", init=False)
     extra: dict[str, Any] = field(default_factory=dict)
 
     # derived classes set their schema as class parameter
     def __init_subclass__(cls):
-        assert cls.schema != Attribute.schema, f"Subclassed attributes must have unique schemas"
+        assert (
+            cls.schema != Attribute.schema
+        ), "Subclassed attributes must have unique schemas"
         assert cls.schema not in SCHEMAS, f"{cls.schema} already registered."
         SCHEMAS[cls.schema] = cls
 
     def matches(self, *args, **kwargs):
         return False
-    
+
     @staticmethod
-    def from_dict(json_dict: dict[str, Any]) -> List['Attribute']:
+    def from_dict(json_dict: dict[str, Any]) -> List["Attribute"]:
         att_type: str = json_dict.pop("@type")
         if att_type.startswith("x-"):
             return []
@@ -30,13 +33,10 @@ class Attribute:
             logger.warn(f"Cannot parse type {att_type}")
             return []
 
-        return_attr: 'Attribute' = Cls(**{
-            key: json_dict[key]
-            for key in json_dict
-            if not key.startswith("x-")
-        })
+        return_attr: "Attribute" = Cls(
+            **{key: json_dict[key] for key in json_dict if not key.startswith("x-")}
+        )
         for key in json_dict:
             if key.startswith("x-"):
                 return_attr.extra[key] = json_dict[key]
         return [return_attr]
-
--- siibra/features_beta/attributes/meta_attributes.py
+++ siibra/features_beta/attributes/meta_attributes.py
@@ -13,29 +13,32 @@ _m = Memory(CACHE.folder, verbose=False)
 
 
 def all_words_in_string(s1: str, s2: str):
-    """ returns true if all words from string s1 are found in  string s2. """
+    """returns true if all words from string s1 are found in  string s2."""
     return all(w in s2.lower() for w in s1.lower())
 
 
 @dataclass
 class MetaAttribute(Attribute):
-    schema="siibra/attr/meta"
+    schema = "siibra/attr/meta"
 
 
 @dataclass
 class ModalityAttribute(MetaAttribute):
-    schema="siibra/attr/meta/modality"
-    name: str
+    schema = "siibra/attr/meta/modality"
+    name: str = None
 
     @staticmethod
     def _GetAll():
         from ...configuration import Configuration
         from ..feature import DataFeature
-        cfg = Configuration()
-        return {attr.name for _, s in cfg.specs.get("siibra/feature/v0.2")
-                for attr in DataFeature(**s).attributes
-                if isinstance(attr, ModalityAttribute)}
 
+        cfg = Configuration()
+        return {
+            attr.name
+            for _, s in cfg.specs.get("siibra/feature/v0.2")
+            for attr in DataFeature(**s).attributes
+            if isinstance(attr, ModalityAttribute)
+        }
 
     def matches(self, *args, modality=None, **kwargs):
         if isinstance(modality, str) and all_words_in_string(modality, self.name):
@@ -45,71 +48,84 @@ class ModalityAttribute(MetaAttribute):
 
 @dataclass
 class AuthorAttribute(MetaAttribute):
-    schema="siibra/attr/meta/author"
-    name: str
+    schema = "siibra/attr/meta/author"
+    name: str = None
     affiliation: str = ""
 
     def matches(self, *args, name=None, affiliation=None, **kwargs):
         if isinstance(name, str) and all_words_in_string(self.name, name):
             return True
-        if isinstance(affiliation, str) and all_words_in_string(self.affiliation, affiliation):
+        if isinstance(affiliation, str) and all_words_in_string(
+            self.affiliation, affiliation
+        ):
             return True
         return super().matches(*args, name=name, affiliation=affiliation, **kwargs)
 
 
 @dataclass
 class DoiAttribute(MetaAttribute):
-    schema="siibra/attr/meta/doi"
-    url: str
+    schema = "siibra/attr/meta/doi"
+    url: str = None
 
 
 @dataclass
 class RegionSpecAttribute(MetaAttribute):
-    schema="siibra/attr/meta/regionspec"
-    name: str
+    schema = "siibra/attr/meta/regionspec"
+    name: str = None
 
     @staticmethod
     @_m.cache
     def Matches(regionspec: str, parcellation: str, region: str) -> bool:
-        found_region = _parcellation.Parcellation.registry()[parcellation].get_region(region)
+        found_region = _parcellation.Parcellation.registry()[parcellation].get_region(
+            region
+        )
         return found_region.matches(regionspec)
-
 
     def matches(self, first_arg=None, *args, region=None, parcellation=None, **kwargs):
         if isinstance(first_arg, _region.Region):
             region = first_arg
         if isinstance(region, _region.Region):
-            return RegionSpecAttribute.Matches(self.name, region.parcellation.name, region.name)
+            return RegionSpecAttribute.Matches(
+                self.name, region.parcellation.name, region.name
+            )
         if isinstance(region, str):
-            assert parcellation, f"If region is supplied as a string, parcellation must be defined!"
-            assert isinstance(parcellation, (str, _parcellation.Parcellation)), f"parcellation must be of type str or Parcellation"
+            assert (
+                parcellation
+            ), "If region is supplied as a string, parcellation must be defined!"
+            assert isinstance(
+                parcellation, (str, _parcellation.Parcellation)
+            ), "parcellation must be of type str or Parcellation"
             if isinstance(parcellation, _parcellation.Parcellation):
                 parcellation = parcellation.name
             return RegionSpecAttribute.Matches(self.name, parcellation, region)
         return super().matches(first_arg, *args, **kwargs)
-    
+
+
 @dataclass
 class PointAttribute(MetaAttribute):
-    schema="siibra/attr/meta/point"
-    space_id: str
+    schema = "siibra/attr/meta/point"
+    space_id: str = None
     coordinate: list[float] = field(default_factory=list)
 
-    def matches(self, first_arg=None, *args, location: Location=None, **kwargs):
-        
+    def matches(self, first_arg=None, *args, location: Location = None, **kwargs):
+
         if isinstance(first_arg, Location):
             location = first_arg
-        if location and location.intersects(Point(self.coordinate, space=self.space_id)):
+        if location and location.intersects(
+            Point(self.coordinate, space=self.space_id)
+        ):
             return True
         return super().matches(first_arg, *args, **kwargs)
 
+
 @dataclass
 class PointSetAttribute(MetaAttribute):
-    schema="siibra/attr/meta/pointset"
-    space_id: str
+    schema = "siibra/attr/meta/pointset"
+    space_id: str = None
     coordinates: list[list[float]] = field(default_factory=list)
 
-    def matches(self, first_arg=None, *args, location: Location=None, **kwargs):
-        
+    def matches(self, first_arg=None, *args, location: Location = None, **kwargs):
+
         if isinstance(first_arg, Location):
             location = first_arg
         if location and location.intersects(
@@ -121,7 +137,7 @@ class PointSetAttribute(MetaAttribute):
 
 @dataclass
 class PolylineDataAttribute(MetaAttribute):
-    schema: str = 'siibra/attr/meta/polyline'
+    schema: str = "siibra/attr/meta/polyline"
     closed: bool = False
     space_id: str = None
     coordinates: list[list[float]] = field(default_factory=list)
@@ -129,8 +145,8 @@ class PolylineDataAttribute(MetaAttribute):
 
 @dataclass
 class SpeciesSpecAttribute(MetaAttribute):
-    schema="siibra/attr/meta/speciesspec"
-    name: str
+    schema = "siibra/attr/meta/speciesspec"
+    name: str = None
 
     def matches(self, *args, species=None, **kwargs):
         if isinstance(species, str) and all_words_in_string(self.species, species):
@@ -142,8 +158,8 @@ class SpeciesSpecAttribute(MetaAttribute):
 
 @dataclass
 class EbrainsAttribute(MetaAttribute):
-    schema="siibra/attr/meta/ebrains"
-    ids: Dict[str, str]
+    schema = "siibra/attr/meta/ebrains"
+    ids: Dict[str, str] = None
 
 
 __all__ = [T.__name__ for T in MetaAttribute.__subclasses__()]
