0bd18908ac1dfafd099a944cb8d37a4ca48b66cb
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-05-14 15:38:00 +0200

    Revert "test: remove region('julich 3', 'hoc1 left') from image test due to masking (data) issue"
    This reverts commit 99e140b03539358ffd7e3ab341f40cecf732d683.

--- e2e/features/image/test_image.py
+++ e2e/features/image/test_image.py
@@ -8,7 +8,8 @@ results = [
     (siibra.features.get(siibra.get_template("big brain"), "CellbodyStainedSection"), 145),
     (siibra.features.get(siibra.get_template("big brain"), "CellBodyStainedVolumeOfInterest"), 2),
     (siibra.features.get(siibra.get_template("mni152"), "image", restrict_space=True), 4),
-    (siibra.features.get(siibra.get_template("mni152"), "image", restrict_space=False), 12),
+    (siibra.features.get(siibra.get_template("mni152"), "image", restrict_space=False), 13),
+    (siibra.features.get(siibra.get_region('julich 3', 'hoc1 left'), "CellbodyStainedSection"), 47),
     (siibra.features.get(siibra.get_region('julich 2.9', 'hoc1 left'), "CellbodyStainedSection"), 41)
 ]
 features = [f for fts, _ in results for f in fts]
