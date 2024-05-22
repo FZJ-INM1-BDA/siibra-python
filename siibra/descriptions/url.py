55d8c4933cedac7ce6b53c6132772f178b3b76e7
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-05-22 16:57:32 +0200

    Update url DescriptionItem with version and satisfy flake8

--- siibra/descriptions/url.py
+++ siibra/descriptions/url.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 
-from ..concepts import attribute
 from .base import Description
+
 
 @dataclass
 class Url(Description):
-    schema = "siibra/attr/desc/url"
+    schema = "siibra/attr/desc/url/v0.1"
     value: str = None
