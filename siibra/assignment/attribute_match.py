cb76b4dd5a86756a5d8886cd717055f2897820e4
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-05-22 16:56:24 +0200

    make regionspec match more comprehensive. Satisfy Flake8
    Note: Cannot add fuzzy match because the operation should be commutable. Regionspec to region comparison can contain the fuzzy match but region is an AttributeCollection not an Atrtribute. Requires further discussion

--- siibra/assignment/attribute_match.py
+++ siibra/assignment/attribute_match.py
@@ -30,8 +30,9 @@ def match(attra: Attribute, attrb: Attribute) -> bool:
 # TODO document me
 COMPARE_ATTR_DICT: Dict[Tuple[Type[Attribute], ...], Tuple[Callable, bool]] = {}
 
+
 def register_attr_comparison(attra_type: Type[Attribute], attrb_type: Type[Attribute]):
-    
+
     def outer(fn):
         forward_key = attra_type, attrb_type
         backward_key = attrb_type, attra_type
@@ -42,45 +43,44 @@ def register_attr_comparison(attra_type: Type[Attribute], attrb_type: Type[Attri
         @wraps(fn)
         def inner(*args, **kwargs):
             return fn(*args, **kwargs)
-        
+
         COMPARE_ATTR_DICT[forward_key] = inner, False
         COMPARE_ATTR_DICT[backward_key] = inner, True
 
         return inner
-    return outer
 
+    return outer
 
 
 @register_attr_comparison(Modality, Modality)
 def compare_modality(mod1: Modality, mod2: Modality):
     return mod1.value.lower() == mod2.value.lower()
 
+
 @register_attr_comparison(RegionSpec, RegionSpec)
 def compare_regionspec(regspec1: RegionSpec, regspec2: RegionSpec):
-    if regspec1.value == regspec2.value:
-        return True
-    # TODO implement fuzzy match
+    return regspec1.value.lower().strip() == regspec2.value.lower().strip()
 
 
 @register_attr_comparison(BBox, BBox)
 def compare_bbox_to_bbox(bbox1: BBox, bbox2: BBox):
     if bbox2.space_id != bbox1.space_id:
-        raise InvalidAttrCompException(f"bbox and image are in different space. Cannot compare the two")
+        raise InvalidAttrCompException("bbox and image are in different space. Cannot compare the two")
     return BBox.intersect_box(bbox1, bbox2) is not None
+
 
 @register_attr_comparison(Pt, BBox)
 def compare_pt_to_bbox(pt: Pt, bbox: BBox):
     if bbox.space_id != pt.space_id:
-        raise InvalidAttrCompException(f"bbox and image are in different space. Cannot compare the two")
+        raise InvalidAttrCompException("bbox and image are in different space. Cannot compare the two")
     minpoint = np.array(bbox.minpoint)
     maxpoint = np.array(bbox.maxpoint)
     pt = np.array(pt.coordinate)
 
-    return np.all(minpoint <= pt) and np.all(pt <= maxpoint) 
+    return np.all(minpoint <= pt) and np.all(pt <= maxpoint)
 
 
 # TODO implement
-# 
 # @register_attr_comparison(BBox, Image)
 # def compare_bbox_to_image(bbox: BBox, image: Image):
 #     if image.space_id != bbox.space_id:
@@ -98,12 +98,12 @@ def compare_pt_to_image(pt: Pt, image: Image):
 def compare_ptcloud_to_image(ptcloud: PointCloud, image: Image):
     intersection = intersect_ptcld_image(ptcloud=ptcloud, image=image)
     return len(intersection.coordinates) > 0
-    
+
 
 def intersect_ptcld_image(ptcloud: PointCloud, image: Image) -> PointCloud:
     if image.space_id != ptcloud.space_id:
-        raise InvalidAttrCompException(f"ptcloud and image are in different space. Cannot compare the two.")
-    
+        raise InvalidAttrCompException("ptcloud and image are in different space. Cannot compare the two.")
+
     value_outside = 0
 
     img = image.data
@@ -128,5 +128,5 @@ def intersect_ptcld_image(ptcloud: PointCloud, image: Image) -> PointCloud:
     values[~inside] = value_outside
 
     inside = list(np.where(values != value_outside)[0])
-    
+
     return replace(ptcloud, coordinates=[ptcloud.coordinates[i] for i in inside])
