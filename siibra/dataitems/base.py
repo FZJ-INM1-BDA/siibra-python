3d1f33dd0bd8a9a6f30da8ae6d1a5d5eaaa7c1cd
Xiao Gui <xgui3783@gmail.com>
2024-05-27 11:12:53 +0200

    fix: data archive

--- siibra/dataitems/base.py
+++ siibra/dataitems/base.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass, field
 from typing import TypedDict
+from ..concepts.attribute import Attribute
+
 
 class Archive(TypedDict):
     label: int = None
     file: str = None
+    format: str = None
 
-from ..concepts.attribute import Attribute
 
 @dataclass
 class Data(Attribute):
