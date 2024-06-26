e258c79a2ee5661fb4ce130568226fc66637b54f
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-06-26 13:36:33 +0200

    remove regionspec repr and str

--- siibra/descriptions/regionspec.py
+++ siibra/descriptions/regionspec.py
@@ -8,14 +8,6 @@ class RegionSpec(Description):
     schema = "siibra/attr/desc/regionspec/v0.1"
     parcellation_id: str = None
 
-    def __str__(self) -> str:
-        return (
-            f"RegionSpec<{self.value!r}, parcellation_id='{self.parcellation_id[-5:]}'>"
-        )
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
     def __hash__(self) -> int:
         return hash(f"{self.parcellation_id}{self.value}")
 
