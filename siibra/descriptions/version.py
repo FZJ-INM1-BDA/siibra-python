2be3bc5e8da0dbfe6288e404dee2dc62d48fde4a
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-07-15 13:00:42 +0200

    `Version(Description)` should use `Packaging.version` for accurate comparison

--- siibra/descriptions/version.py
+++ siibra/descriptions/version.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from packaging.version import Version as PackagingVersion
 
 from .base import Description
 
@@ -8,3 +9,7 @@ class Version(Description):
     schema = "siibra/attr/desc/version/v0.1"
     prev_id: str = None  # None if there is no previous
     next_id: str = None  # None if there is no next
+
+    def __lt__(self, other: "Version"):
+        assert isinstance(other, Version), TypeError(f"'>' not supported between instances of '{type(self)}' and '{type(other)}'")
+        return PackagingVersion(self.value) < PackagingVersion(other.value)
