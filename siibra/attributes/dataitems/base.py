5a07d79fbea60c428f0e539c569f619bb939c264
Xiao Gui <xgui3783@gmail.com>
2024-08-23 10:28:35 +0200

    WIP: rework data attribute incl todos

--- siibra/attributes/dataitems/base.py
+++ siibra/attributes/dataitems/base.py
@@ -13,9 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 import requests
+from typing import Generic, TypeVar, List, Callable
 
 try:
     from typing import TypedDict
@@ -63,21 +64,207 @@ def get_bytesio_from_url(url: str, archive_options: Archive = None) -> bytes:
     return repo.get(filename)
 
 
+src_remote_tar = {
+    "type": "src/remotetar",
+    "tar": "http://foo/bar.tar",
+    "filename": "bazz/bud.txt",
+}
+
+src_local_tar = {
+    "type": "src/localtar",
+    "tar": "/tmp/bar.tar",
+    "filename": "bazz/bud.txt",
+}
+
+src_remote_file = {"type": "src/file", "url": "http://foo/bud.txt"}
+
+src_local_file = {"type": "src/file", "url": "/foo/bud.txt"}
+
+codec_gzip = {
+    "type": "codec/gzip",
+    "op": "decompress",
+}
+
+
+codec_slice = {"type": "codec/slice", "offset": 200, "bytes": 24}
+
+read_nib = {"type": "read/nibabel"}
+
+read_csv = {"type": "read/csv"}
+
+codec_vol_slice = {
+    "type": "codec/vol/slice",
+    "param": [],
+}
+
+codec_vol_mask = {"type": "codec/vol/mask", "threshold": 0.1}
+
+codec_vol_extract_label = {"type": "codec/vol/extractlabel", "labels": [1, 5, 11]}
+
+codec_vol_to_bytes = {"type": "codec/vol/tobytes"}
+
+dst_return = {"type": "dst/return"}
+
+dst_save_to_file = {"type": "dst/save", "filename": "foo.nii.gz"}
+
+dst_upload = {
+    "type": "dst/upload/dataproxy",
+    "token": "ey...",
+    "bucket": "mybucket",
+    "filename": "foobar.nii.gz",
+}
+
+"""
+proposal, DataProvider have a series of "step"'s, which describe everything, such as:
+
+- how data is retrieved
+- how data is transformed
+- how data is saved/returned
+
+to note:
+
+- In the current implementation, Step is modelled as a class. So each subclass implement a `run` method.
+- The run method takes a positional argument (input), a cfg kwarg, and arbitary kwargs.
+- Each step *should* have proper type annotation (so static/runtime type check can be performed, to avoid runtime issue)
+- The steps may choose to ignore the input (would be the case for source steps, such as fetching )
+- similarly, the steps may choose to not return anything (i.e. return None), if it is a final step (e.g. save to a file)
+
+we can also create a text description on a "dry-run". e.g.
+
+1. fetch tar file from http://, extract and get filename
+2. gunzip the file
+3. read it as nifti with nibabel
+4. mask it with threshold 0.3
+5. convert the nifti to bytes
+6. gzip the result
+7. save to my.nii.gz
+
+potentially, we can do things like forking/merging etc (need to be a bit careful about mutability of data)
+
+"""
+
+import nibabel as nib
+from typing import Dict, Type
+
+
+# TODO rename data operation
+# TODO neuroglancer is different to nifti
+# TODO rename retrieval/ -> dataops/
+# TODO add transformops to dataops/
+class DataOp:
+    input: None
+    output: None
+    desc: str = "Noop"
+
+    step_register: Dict[str, Type["DataOp"]] = {}
+
+    def run(self, input, *, cfg, **kwargs):
+        return input
+
+    def __init_subclass__(cls, type: str = None) -> None:
+        if not type:
+            return
+        assert type not in cls.step_register, f"Already registered {type}"
+        cls.step_register[type] = cls
+
+    @classmethod
+    def get_runner(cls, step: Dict):
+        type = step.get("type")
+        assert type in cls.step_register, f"{type} not found in step register"
+        return cls.step_register[type]
+
+    @classmethod
+    def describe(cls, steps: List[Dict]):
+        descs = []
+        for idx, step in enumerate(steps, start=1):
+            runner = cls.get_runner(step)
+            descs.append(f"{idx} - {runner.desc.format(step)}")
+        return "\n".join(descs)
+
+
+class ReadAsNifti(DataOp, type="read/nibabel"):
+    input: bytes
+    output: nib.nifti1.Nifti1Image
+    desc = "Reads bytes into nifti"
+
+    def run(self, input, *, cfg, **kwargs):
+        assert isinstance(input, bytes)
+        import nibabel as nib
+
+        return nib.nifti1.Nifti1Image.from_bytes(input)
+
+
+class NiftiCodec(DataOp):
+    input: nib.nifti1.Nifti1Image
+    output: nib.nifti1.Nifti1Image
+    desc = "Transforms nifti to nifti"
+
+
+class NiftiMask(NiftiCodec, type="codec/vol/mask"):
+    desc = "Mask nifti according according to spec {cfg}"
+
+    def run(self, input, *, cfg, **kwargs):
+        import nibabel as nib
+
+        threshold = cfg.get("threshold")
+        assert isinstance(input, nib.nifti1.Nifti1Image)
+        dataobj = input.dataobj
+        dataobj = dataobj[dataobj > threshold]
+        return nib.nifti1.Nifti1Image(dataobj, affine=input.affine, header=input.header)
+
+    @classmethod
+    def from_threshold(cls, threshold=0):
+        return {"type": "codec/vol/mask", "threshold": threshold}
+
+
+from siibra.atlases.parcellationmap import Map
+from dataclasses import replace
+
+
+# TODO should be renamed DataProvider
 @dataclass
 class Data(Attribute):
     schema: str = "siibra/attr/data"
     key: str = None
+
+    # url can be from remote (http) or localfile
     url: str = None
     archive_options: Archive = None
 
-    def get_data(self) -> bytes:
-        """
-        If the data is provided in an archived format, it is decoded using the
-        otherwise bytes are returned without additional steps. This is so that
-        the subclasses do not need to implement their own.
+    transformation_ops: List = field(default_factory=list)
 
-        Usage
-        -----
-        For subclasses, call super().get_data() -> bytes
-        """
-        return get_bytesio_from_url(self.url, self.archive_options)
+    @property
+    def retrieval_ops(self):
+        assert self.url, f"url must be defined"
+
+        if not self.archive_options:
+            return [{"type": "src/file", "url": self.url}]
+
+        if self.archive_options["format"] == "tar":
+            return [
+                {
+                    "type": "src/remotetar",
+                    "tar": self.url,
+                    "filename": self.archive_options["file"],
+                }
+            ]
+        if self.archive_options["format"] == "zip":
+            return [
+                {
+                    "type": "src/remotezip",
+                    "tar": self.url,
+                    "filename": self.archive_options["file"],
+                }
+            ]
+        raise NotImplementedError
+
+    # TODO cache this step
+    def get_data(self, **kwargs):
+        result = None
+        for step in [
+            *self.retrieval_ops,
+            *self.transformation_ops,
+        ]:
+            runner = DataOp.get_runner(step)
+            result = runner.run(result, **kwargs)
+        return result
