e44a38328f3d97d0e7665f058d246c0731669bc2
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-09-11 10:36:56 +0200

    use env vars for `MEMORY_HUNGRY` and `KEEP_LOCAL_CACHE`

--- siibra/commons/conf.py
+++ siibra/commons/conf.py
@@ -1,5 +1,5 @@
 import os
 
-KEEP_LOCAL_CACHE = 1
-MEMORY_HUNGRY = 1
+KEEP_LOCAL_CACHE = int(os.getenv("MEMORY_HUNGRY", 1))
+MEMORY_HUNGRY = int(os.getenv("MEMORY_HUNGRY", 1))
 SIIBRA_MAX_FETCH_SIZE_GIB = float(os.getenv("SIIBRA_MAX_FETCH_SIZE_GIB", 0.2))
