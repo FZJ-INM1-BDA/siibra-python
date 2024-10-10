d07eb2d43ca6c8141bb5ec2e8c367428a05ecc38
Xiao Gui <xgui3783@gmail.com>
2024-10-10 18:04:16 +0200

    fix: notebook 5 (layerwise modified silver staining -> cell body density) fix: big brain profile

--- siibra/factory/livequery/bigbrain_profile.py
+++ siibra/factory/livequery/bigbrain_profile.py
@@ -15,7 +15,7 @@
 
 import numpy as np
 import pandas as pd
-from typing import Iterator
+from typing import Iterator, Union
 import requests
 from io import BytesIO
 from nibabel import GiftiImage
@@ -34,6 +34,7 @@ from ...attributes.locations.layerboundary import (
     X_PRECALCULATED_BOUNDARY_KEY,
     LAYERS,
 )
+from ...attributes.datarecipes.volume import ImageRecipe
 from ...attributes.locations import intersect, PointCloud, Point, PolyLine
 from ...exceptions import UnregisteredAttrCompException, InvalidAttrCompException
 
@@ -45,6 +46,7 @@ modalities_of_interest = [
 
 X_BIGBRAIN_PROFILE_VERTEX_IDX = "x-siibra/bigbrainprofile/vertex-idx"
 X_BIGBRAIN_LAYERWISE_INTENSITY = "x-siibra/bigbrainprofile/layerwiseintensity"
+BIGBRAIN_ID = "minds/core/referencespace/v1.0.0/a1655b99-82f1-420f-a3c2-fe80fd4c8588"
 
 
 @register_modalities()
@@ -61,121 +63,122 @@ class BigBrainProfile(LiveQuery[Feature], generates=Feature):
             return
         valid_depths, boundary_depths, profiles, vertices = get_all()
         bigbrain_vertices = PointCloud(
-            space_id="minds/core/referencespace/v1.0.0/a1655b99-82f1-420f-a3c2-fe80fd4c8588",
+            space_id=BIGBRAIN_ID,
             coordinates=vertices.tolist(),
         )
         root_coords = np.array(bigbrain_vertices.coordinates)
         dtype = {"names": ["x", "y", "z"], "formats": [root_coords.dtype] * 3}
         root_coords = root_coords.view(dtype)
 
-        input_attrs = [attr for attr_col in self.input for attr in attr_col.attributes]
+        image_recipes = [
+            recipe
+            for recipes in self.find_attributes(ImageRecipe)
+            for recipe in recipes
+        ]
 
-        for input_attr in input_attrs:
-            attributes = [*modalities_of_interest]
-            try:
-                matched_verts = intersect(input_attr, bigbrain_vertices)
-            except UnregisteredAttrCompException:
-                continue
-            except InvalidAttrCompException:
-                continue
-            if matched_verts is None:
-                continue
-            if isinstance(matched_verts, Point):
-                matched_verts = PointCloud(
-                    space_id=matched_verts.space_id,
-                    coordinates=[matched_verts.coordinate],
-                )
-            assert isinstance(matched_verts, PointCloud)
-            attributes.append(matched_verts)
+        if len(image_recipes) == 0:
+            return
 
-            matched_coords = np.array(matched_verts.coordinates)
-            matched_coords = matched_coords.view(dtype)
-            coordidx_in_matched = np.in1d(root_coords, matched_coords)
-            coordidx = np.argwhere(coordidx_in_matched)[:, 0]
+        if len(image_recipes) > 1:
+            logger.info(f"Found {len(image_recipes)}, using the first one only.")
 
-            matched_profiles = profiles[coordidx]
-            matched_boundary_depths = boundary_depths[coordidx]
-            N = matched_profiles.shape[1]
-            prange = np.arange(N)
-            layer_labels = 7 - np.array(
+        image_recipe = image_recipes[0]
+
+        warped_vertices = bigbrain_vertices.warp(image_recipe.space_id)
+        indices, values = image_recipe.lookup_points(warped_vertices)
+        filtered_indices = np.array(indices)[np.nonzero(values)]
+
+        if len(filtered_indices) == 0:
+            return
+
+        filtere_ptcld = PointCloud(
+            coordinates=vertices[filtered_indices],
+            space_id=BIGBRAIN_ID,
+        )
+
+        attributes = [*modalities_of_interest, filtere_ptcld]
+
+        matched_profiles = profiles[filtered_indices]
+        matched_boundary_depths = boundary_depths[filtered_indices]
+        N = matched_profiles.shape[1]
+        prange = np.arange(N)
+        layer_labels = 7 - np.array(
+            [
                 [
-                    [
-                        np.array(
-                            [
-                                [(prange < T) * 1]
-                                for i, T in enumerate((b * N).astype("int"))
-                            ]
-                        )
-                        .squeeze()
-                        .sum(0)
-                    ]
-                    for b in matched_boundary_depths
+                    np.array(
+                        [
+                            [(prange < T) * 1]
+                            for i, T in enumerate((b * N).astype("int"))
+                        ]
+                    )
+                    .squeeze()
+                    .sum(0)
                 ]
-            ).reshape((-1, 200))
-            means = [
-                matched_profiles[layer_labels == layer].mean() for layer in range(1, 7)
+                for b in matched_boundary_depths
             ]
-            std = [
-                matched_profiles[layer_labels == layer].std() for layer in range(1, 7)
-            ]
+        ).reshape((-1, 200))
+        means = [
+            matched_profiles[layer_labels == layer].mean() for layer in range(1, 7)
+        ]
+        std = [matched_profiles[layer_labels == layer].std() for layer in range(1, 7)]
 
-            dataframe = pd.DataFrame(
-                np.array([means, std]).T,
-                columns=["mean", "std"],
-                index=LAYERS[1:-1],
+        dataframe = pd.DataFrame(
+            np.array([means, std]).T,
+            columns=["mean", "std"],
+            index=LAYERS[1:-1],
+        )
+
+        hashed_io = md5(
+            (json.dumps(image_recipe.ops) + json.dumps(asdict(filtere_ptcld))).encode(
+                "utf-8"
             )
+        ).hexdigest()
 
-            hashed_io = md5(
-                (
-                    json.dumps(asdict(input_attr)) + json.dumps(asdict(matched_verts))
-                ).encode("utf-8")
-            ).hexdigest()
+        filename = CACHE.build_filename(hashed_io, suffix=".csv")
+        dataframe.to_csv(filename)
+        input_attr = TabularDataRecipe(
+            url=filename,
+            name=SUMMARY_NAME,
+            plot_options={"y": "mean", "yerr": "std", "kind": "bar"},
+        )
+        attributes.append(input_attr)
 
-            filename = CACHE.build_filename(hashed_io, suffix=".csv")
-            dataframe.to_csv(filename)
-            input_attr = TabularDataRecipe(
+        for index in filtered_indices.tolist():
+            _profile = profiles[index]
+            depth = np.arange(0.0, 1.0, 1.0 / (profiles[index].shape[0]))
+
+            df = pd.DataFrame(_profile, index=depth)
+            filename = CACHE.build_filename(hashed_io, suffix=f"-pr-{index}.csv")
+            df.to_csv(filename)
+
+            tabular_attr = TabularDataRecipe(
                 url=filename,
-                name=SUMMARY_NAME,
-                plot_options={"y": "mean", "yerr": "std", "kind": "bar"},
+                name=f"Intensity profile for {bigbrain_vertices[index]}",
+                extra={
+                    X_BIGBRAIN_PROFILE_VERTEX_IDX: index,
+                },
             )
-            attributes.append(input_attr)
 
-            for index in coordidx.tolist():
-                _profile = profiles[index]
-                depth = np.arange(0.0, 1.0, 1.0 / (profiles[index].shape[0]))
+            # TODO fix to port/leverage the ops mechanism, rather than
+            # this adhoc mess
+            layer_boundary = LayerBoundary(
+                extra={
+                    X_PRECALCULATED_BOUNDARY_KEY: [
+                        PolyLine(
+                            coordinates=[
+                                (value, 0, 0)
+                                for value in boundary_depths[index].tolist()
+                            ],
+                            space_id=None,
+                        )
+                    ],
+                    X_BIGBRAIN_PROFILE_VERTEX_IDX: index,
+                }
+            )
+            attributes.append(tabular_attr)
+            attributes.append(layer_boundary)
 
-                df = pd.DataFrame(_profile, index=depth)
-                filename = CACHE.build_filename(hashed_io, suffix=f"-pr-{index}.csv")
-                df.to_csv(filename)
-
-                tabular_attr = TabularDataRecipe(
-                    url=filename,
-                    name=f"Intensity profile for {bigbrain_vertices[index]}",
-                    extra={
-                        X_BIGBRAIN_PROFILE_VERTEX_IDX: index,
-                    },
-                )
-
-                # TODO fix to port/leverage the ops mechanism, rather than
-                # this adhoc mess
-                layer_boundary = LayerBoundary(
-                    extra={
-                        X_PRECALCULATED_BOUNDARY_KEY: [
-                            PolyLine(
-                                coordinates=[
-                                    (value, 0, 0)
-                                    for value in boundary_depths[index].tolist()
-                                ],
-                                space_id=None,
-                            )
-                        ],
-                        X_BIGBRAIN_PROFILE_VERTEX_IDX: index,
-                    }
-                )
-                attributes.append(tabular_attr)
-                attributes.append(layer_boundary)
-
-            yield Feature(attributes=attributes)
+        yield Feature(attributes=attributes)
 
 
 REPO = "https://github.com/kwagstyl/cortical_layers_tutorial/raw/main"
