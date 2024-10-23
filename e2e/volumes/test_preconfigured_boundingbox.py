35344332fe0aa5471dcdf9f1d7bd7b1837817b57
Xiao Gui <xgui3783@gmail.com>
2024-10-23 12:26:22 +0200

    fix: e2e test for preconf bbox

--- e2e/volumes/test_preconfigured_boundingbox.py
+++ e2e/volumes/test_preconfigured_boundingbox.py
@@ -2,30 +2,32 @@ import siibra
 from siibra.features.image.image import Image
 from siibra.volumes.volume import Volume
 import pytest
+from itertools import repeat
 
-space_vols = [v for s in siibra.spaces for v in s.volumes]
 map_vols = [v for m in siibra.maps for v in m.volumes]
 imagefeatures = [
     feat
     for ftype in siibra.features.Feature._SUBCLASSES[Image]
     for feat in ftype._get_instances()
 ]
-volumes = space_vols + map_vols + imagefeatures
+volumes = list(zip(map_vols, repeat(True))) + list(zip(imagefeatures, repeat(False)))
 
 
-@pytest.mark.parametrize("volume", volumes)
-def test_onthefly_and_preconfig_bboxes(volume: Volume):
+@pytest.mark.parametrize("volume, clip_flag", map_vols)
+def test_onthefly_and_preconfig_bboxes(volume: Volume, clip_flag: bool):
     configured_bbox = volume._boundingbox
     if configured_bbox is None:
         pytest.skip(f"No preconfigured BoundingBox for {volume} is found.")
     volume._boundingbox = None
-    kwargs = {"clip": True}
+    kwargs = {"clip": clip_flag}
     if "neuroglancer/precomputed" in volume.providers:
-        kwargs.update({
-            "clip": False,
-            "resolution_mm": -1,
-            "format": "neuroglancer/precomputed",
-            "max_bytes": 2 * 1024**3
-        })
+        kwargs.update(
+            {
+                "clip": False,
+                "resolution_mm": -1,
+                "format": "neuroglancer/precomputed",
+                "max_bytes": 2 * 1024**3,
+            }
+        )
     bbox = volume.get_boundingbox(**kwargs)
-    assert configured_bbox == bbox, f'{volume}'
+    assert configured_bbox == bbox, f"{volume}"
