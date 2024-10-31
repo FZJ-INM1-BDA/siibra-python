ef0e542fa52b250fe37b589573ceff27b0033aff
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-10-31 09:52:29 +0100

    test: skip centroid computation test for surface maps

--- e2e/volumes/test_compute_centroids.py
+++ e2e/volumes/test_compute_centroids.py
@@ -8,4 +8,8 @@ preconfiugres_maps = list(siibra.maps)
 
 @pytest.mark.parametrize("mp", preconfiugres_maps)
 def test_compute_centroids(mp: Map):
+    if not mp.provides_image:
+        pytest.skip(
+            f"Currently, centroid computation for meshes is not implemented. Skipping '{mp.name}'"
+        )
     _ = mp.compute_centroids()
