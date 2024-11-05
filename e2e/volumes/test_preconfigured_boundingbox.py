5188e72e064c44e2b4635b448ed423db46584328
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-11-05 15:33:55 +0100

    test random selection of preconfigured bboxes

--- e2e/volumes/test_preconfigured_boundingbox.py
+++ e2e/volumes/test_preconfigured_boundingbox.py
@@ -1,8 +1,17 @@
+import pytest
+from itertools import repeat
+from time import time
+import os
+import numpy as np
+
 import siibra
 from siibra.features.image.image import Image
 from siibra.volumes.volume import Volume
-import pytest
-from itertools import repeat
+
+TEST_ALL_PRECONF_BBOXES = eval(os.getenv("TEST_ALL_PRECONF_BBOXES", "False"))
+RANDOM_SEED = os.getenv("RANDOM_SEED", None if TEST_ALL_PRECONF_BBOXES else int(time()))
+RANDOM_TEST_COUNT = os.getenv("RANDOM_TEST_COUNT", None if TEST_ALL_PRECONF_BBOXES else 20)
+
 
 map_vols = [v for m in siibra.maps for v in m.volumes]
 imagefeatures = [
@@ -12,8 +21,17 @@ imagefeatures = [
 ]
 volumes = list(zip(map_vols, repeat(True))) + list(zip(imagefeatures, repeat(False)))
 
+if not TEST_ALL_PRECONF_BBOXES:
+    np.random.seed(int(RANDOM_SEED))
+    randomly_selected_volumes = [
+        volumes[i] for i in np.random.randint(0, len(volumes), int(RANDOM_TEST_COUNT))
+    ]
 
-@pytest.mark.parametrize("volume, clip_flag", volumes)
+
+@pytest.mark.parametrize(
+    "volume, clip_flag",
+    volumes if TEST_ALL_PRECONF_BBOXES else randomly_selected_volumes
+)
 def test_onthefly_and_preconfig_bboxes(volume: Volume, clip_flag: bool):
     configured_bbox = volume._boundingbox
     if configured_bbox is None:
