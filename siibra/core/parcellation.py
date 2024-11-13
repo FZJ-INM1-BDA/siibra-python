175bed30f43ce1b95301ee6ab00eb89422cfb3e1
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-11-13 13:09:04 +0100

    Raise if `get_map` can't find any maps instead of logs

--- siibra/core/parcellation.py
+++ siibra/core/parcellation.py
@@ -18,9 +18,12 @@ from . import region
 from ..commons import logger, MapType, Species
 from ..volumes import parcellationmap
 
-from typing import Union, List, Dict
+from typing import Union, List, Dict, TYPE_CHECKING
 import re
 
+
+if TYPE_CHECKING:
+    from .space import Space
 
 # NOTE : such code could be used to automatically resolve
 # multiple matching parcellations for a short spec to the newset version:
@@ -140,7 +143,12 @@ class Parcellation(region.Region, configuration_folder="parcellations"):
                     self._CACHED_MATCHES[spec] = True
         return super().matches(spec)
 
-    def get_map(self, space=None, maptype: Union[str, MapType] = MapType.LABELLED, spec: str = ""):
+    def get_map(
+        self,
+        space: Union[str, "Space"],
+        maptype: Union[str, MapType] = MapType.LABELLED,
+        spec: str = ""
+    ):
         """
         Get the maps for the parcellation in the requested template space.
 
@@ -153,8 +161,8 @@ class Parcellation(region.Region, configuration_folder="parcellations"):
         Parameters
         ----------
         space: Space or str
-            template space specification
-        maptype: MapType
+            reference space specification such as name, id, or a `Space` instance.
+        maptype: MapType or str
             Type of map requested (e.g., statistical or labelled).
             Use MapType.STATISTICAL to request probability maps.
             Defaults to MapType.LABELLED.
@@ -176,19 +184,16 @@ class Parcellation(region.Region, configuration_folder="parcellations"):
             m for m in parcellationmap.Map.registry()
             if m.space.matches(space)
             and m.maptype == maptype
-            and m.parcellation
             and m.parcellation.matches(self)
         ]
         if len(candidates) == 0:
-            logger.error(f"No {maptype} map in {space} available for {str(self)}")
-            return None
+            raise ValueError(f"No '{maptype}' map in '{space}' available for {str(self)}")
         if len(candidates) > 1:
             spec_candidates = [
                 c for c in candidates if all(w.lower() in c.name.lower() for w in spec.split())
             ]
             if len(spec_candidates) == 0:
-                logger.warning(f"'{spec}' does not match any options from {[c.name for c in candidates]}.")
-                return None
+                raise ValueError(f"'{spec}' does not match any options from {[c.name for c in candidates]}.")
             if len(spec_candidates) > 1:
                 logger.warning(
                     f"Multiple maps are available in this specification of space, parcellation, and map type.\n"
