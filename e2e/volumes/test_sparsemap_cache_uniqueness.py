628bf7109647b68b5741c28beef4a03e4a2cdd17
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-12-20 13:30:51 +0100

    test: uniqueness of sparseindex cache prefixes

--- e2e/volumes/test_sparsemap_cache_uniqueness.py
+++ e2e/volumes/test_sparsemap_cache_uniqueness.py
@@ -2,6 +2,14 @@ import siibra
 from siibra.volumes.sparsemap import SparseMap
 
 
+def test_cache_prefixes():
+    sparsemaps = [
+        mp for mp in siibra.maps
+        if isinstance(mp, siibra.volumes.sparsemap.SparseMap)
+    ]
+    assert len(sparsemaps) == len([mp._cache_prefix for mp in sparsemaps])
+
+
 def test_sparsemap_cache_uniqueness():
     mp157 = siibra.get_map("julich 3.0", "colin 27", "statistical", spec="157")
     mp175 = siibra.get_map("julich 3.0", "colin 27", "statistical", spec="175")
