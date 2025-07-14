c2e39801beebd0f18632354d27160203e0a318a2
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2025-07-14 17:31:43 +0200

    Update 005_rodent_pointdists.py

--- examples/milestone_1_1/005_rodent_pointdists.py
+++ examples/milestone_1_1/005_rodent_pointdists.py
@@ -22,7 +22,6 @@ Rodent tracing connectivity distribution
 import siibra
 from nilearn import plotting
 
-
 # %%
 region = siibra.get_region(
     "waxholm rat", "Primary somatosensory area, forelimb representation"
@@ -33,25 +32,21 @@ for f in tracing_conns:
     print(f.name)
 
 # %%
-print(tracing_conns[0].name)
-print(tracing_conns[0].datasets[0].name)
-print(tracing_conns[0].modality)
-tracing_conns[0].data
+print(tracing_conns[2].name)
+print(tracing_conns[2].datasets[0].name)
+print(tracing_conns[2].modality)
+tracing_conns[2].data
 
 # %%
 display = plotting.plot_img(
     img=siibra.get_template("waxholm").fetch(),
     bg_img=None,
     cmap="gray",
-    title=f"red: {tracing_conns[0].name}, blue: {tracing_conns[-1].name}",
-    cut_coords=tracing_conns[0].as_pointcloud().coordinates.mean(axis=0),
+    title=f"red: {tracing_conns[2].name}\nblue: {tracing_conns[4].name}",
+    cut_coords=tracing_conns[2].data.mean(axis=0),
 )
-display.add_markers(
-    tracing_conns[0].as_pointcloud().coordinates, marker_color="r", marker_size=1
-)
-display.add_markers(
-    tracing_conns[-1].as_pointcloud().coordinates, marker_color="b", marker_size=1
-)
+display.add_markers(tracing_conns[2].data, marker_color="r", marker_size=1)
+display.add_markers(tracing_conns[4].data, marker_color="b", marker_size=1)
 
 
 # %%
@@ -62,22 +57,19 @@ for f in tracing_conns:
     print(f.name)
 
 # %%
-print(tracing_conns[0].name)
-print(tracing_conns[0].datasets[0].name)
-print(tracing_conns[0].modality)
-tracing_conns[0].data
+print(tracing_conns[3].name)
+print(tracing_conns[3].datasets[0].name)
+print(tracing_conns[3].modality)
+print(tracing_conns[3].description)
+tracing_conns[3].data
 
 # %%
 display = plotting.plot_img(
     img=siibra.get_template("mouse").fetch(),
     bg_img=None,
     cmap="gray",
-    title=f"red: {tracing_conns[0].name}, blue: {tracing_conns[-1].name}",
-    cut_coords=tracing_conns[0].as_pointcloud().coordinates.mean(axis=0),
+    title=f"red: {tracing_conns[3].name}\nblue: {tracing_conns[7].name}",
+    cut_coords=tracing_conns[3].data.mean(axis=0),
 )
-display.add_markers(
-    tracing_conns[0].as_pointcloud().coordinates, marker_color="r", marker_size=1
-)
-display.add_markers(
-    tracing_conns[-1].as_pointcloud().coordinates, marker_color="b", marker_size=1
-)
+display.add_markers(tracing_conns[3].data, marker_color="r", marker_size=1)
+display.add_markers(tracing_conns[7].data, marker_color="b", marker_size=1)
