61338ff565b3166daef36037647557e9318074d4
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2025-09-23 17:38:59 +0200

    update Macaque high-resolution MRI notebook

--- examples/milestone_1_1/004_macaque_highres_MRI.py
+++ examples/milestone_1_1/004_macaque_highres_MRI.py
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 """
-Macaque receptor mapping and high-resolution MRI
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Macaque high-resolution MRI
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
 
@@ -23,16 +23,22 @@ Macaque receptor mapping and high-resolution MRI
 import siibra
 from nilearn import plotting
 
+# sphinx_gallery_thumbnail_path = '_static/example_thumbnails/macaque_high_res_MRI.png'
+
 # %%
+# Query for mri images registered on MEBRAINS population-based monkey template
 space = siibra.spaces.get("mebrains")
+mri_images = siibra.features.get(space, "mri")
+for f in mri_images:
+    print(f.name)
+    print("Publication:", f.urls)
 
 # %%
-fts = siibra.features.get(space, "mri")
-
-# %%
+# Fetch the template image and overlay 180um resoltion image.
 tmpl_img = space.get_template().fetch(resolution_mm=-1)
+mri_180um = [f for f in mri_images if "180micron" in f.name][0]
 plotting.view_img(
-    fts[1].fetch(resolution_mm=-1),
+    mri_180um.fetch(resolution_mm=-1),
     bg_img=tmpl_img,
     cmap="magma",
     symmetric_cmap=False,
@@ -40,14 +46,24 @@ plotting.view_img(
 )
 
 # %%
+# Or plot without background in full resolution since Mebrains macaque template
+# has 40um resolution
+plotting.view_img(
+    mri_180um.fetch(resolution_mm=-1),
+    bg_img=None,
+    cmap="gray",
+    symmetric_cmap=False,
+    black_bg=True,
+)
+
+# %%
+# Now, select a volume of interest and plot 100um resoltion MRI
 voi = siibra.BoundingBox((-15.50, -21.50, -10.30), (1.70, 0.90, 0.10), space)
-voi_img = fts[0].fetch(voi=voi, resolution_mm=-1)
+mri_100um = [f for f in mri_images if "100micron" in f.name][0]
+voi_img = mri_100um.fetch(voi=voi, resolution_mm=-1)
 plotting.view_img(
     voi_img,
     bg_img=None,
     cmap="gray",
     symmetric_cmap=False,
 )
-
-# %%
-plotting.plot_img(voi_img, bg_img=None, cmap="gray", display_mode="y")
