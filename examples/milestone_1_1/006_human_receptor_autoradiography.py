d7037710fad8f7610f6ab1007f86af5e5516cb01
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2025-09-23 20:10:06 +0200

    Update 006_human_receptor_autoradiography.py

--- examples/milestone_1_1/006_human_receptor_autoradiography.py
+++ examples/milestone_1_1/006_human_receptor_autoradiography.py
@@ -21,30 +21,43 @@ Human receptor autoradiography
 
 # %%
 import siibra
-from nilearn import plotting
+from nilearn import plotting, image
 import matplotlib.pyplot as plt
 
 # sphinx_gallery_thumbnail_path = '_static/example_thumbnails/gaba_autoradiography.png'
 
 # %%
+# Begin by querying for autoradiography volumes registered to MNI 152 2009c
+# Asym Nonl space and select the volume of Gaba/BZ distribution:
 mni152 = siibra.spaces["mni152"]
 autoradiography_images = siibra.features.get(
     mni152, siibra.features.molecular.AutoradiographyVolumeOfInterest
 )
 for f in autoradiography_images:
     print(f.name)
+    if "GABA/BZ" in f.name:
+        gaba_autoradiography = f
 
 # %%
-gaba_autoradiography = autoradiography_images[0]
-print(gaba_autoradiography.modality)
-
+# We can then plot it over the template, however, the volume has a higher
+# resolution. So we fetch in 600um resolution and upsample the template from 1mm
+# to display it
+img = gaba_autoradiography.fetch(resolution_mm=0.6)
+upsampled_template_img = image.resample_to_img(mni152.get_template().fetch(), img)
 plotting.view_img(
-    gaba_autoradiography.fetch(),
+    img,
+    bg_img=upsampled_template_img,
     cmap="magma",
     symmetric_cmap=False,
+    vmin=1e-7,
+    vmax=7500,
 )
 
 # %%
+# We can make use of siibra to directly draw samples from selected parts of the
+# volume. For this example, draw samples from boxes surrounding regions
+# "4p right", "hoc1 right", and "3b right". Then, compare the distributions
+# using matplotlib:
 julich_brain = siibra.parcellations["julich 3.1"]
 regions = [
     julich_brain.get_region(spec) for spec in ["4p right", "hoc1 right", "3b right"]
@@ -56,6 +69,7 @@ for i, r in enumerate(regions):
         voi=r.get_boundingbox(mni152),
         max_bytes=1 * 1024**3,
     )
-    axs[i].hist(gaba_autoradiography.evaluate_points(samples, max_bytes=1 * 1024**3))
+    sample_values = gaba_autoradiography.evaluate_points(samples, max_bytes=1 * 1024**3)
+    axs[i].hist(sample_values, bins=100)
     axs[i].set_title(r.name)
-    axs[i].set_xlim((0, 7500))
+    axs[i].set_xlim((1e-7, 7500))
