8b9d3d39ef8fba076a86f664dba7ef1bc71a252f
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2025-09-23 18:24:46 +0200

    Update 011_hi_res_chenonceau_images.py

--- examples/milestone_1_1/011_hi_res_chenonceau_images.py
+++ examples/milestone_1_1/011_hi_res_chenonceau_images.py
@@ -23,16 +23,49 @@ import siibra
 from nilearn import plotting
 
 # %%
+# Query for images on MNI 152 2009c Aym Nonl space and filter out the ones from
+# Chenonceau dataset
 mni152 = siibra.spaces["mni152"]
-image_features = siibra.features.get(
-    mni152, siibra.features.generic.Image
-)
+image_features = [
+    f
+    for f in siibra.features.get(mni152, siibra.features.generic.Image)
+    if "Chenonceau" in f.name
+]
 for f in image_features:
+    print(f.name)
     print(f.modality)
 
 # %%
+# As we see from above, there are several modalities to choose from. For
+# demonstration, fetch the image for "proton density" modality and plot it
 proton_density = siibra.features.get(
     mni152, siibra.features.generic.Image, modality="proton density"
 )[0]
 pd_img = proton_density.fetch()
-plotting.plot_img(pd_img, cmap="magma")
+plotting.plot_img(pd_img, cmap="magma", title=proton_density.name)
+
+# %%
+# The set of images integrated also contain T2-weighted anatomical images
+# of the Chenonceau brain. This can be used to overlay other modalities. Since
+# MNI 152 template is only in 1mm resolution, the the underlying anatomoical
+# structure is not present as it would be with Chenonceau 100um or 150um
+# images.
+t2_weighted_150um = [
+    f
+    for f in siibra.features.get(
+        mni152,
+        siibra.features.generic.Image,
+        modality="T2-weighted (T2w) image",
+    )
+    if "150 micrometer" in f.name
+][0]
+plotting.view_img(
+    pd_img,
+    bg_img=t2_weighted_150um.fetch(),
+    cmap="magma",
+    symmetric_cmap=False,
+    title=f"{proton_density}\non {t2_weighted_150um}",
+    vmin=1e-7,
+    vmax=0.15,
+    opacity=0.5,
+)
