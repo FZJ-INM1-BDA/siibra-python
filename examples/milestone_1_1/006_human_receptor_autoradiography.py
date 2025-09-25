cd584d1f2039c9c23fa12515341bd1b5d2b9948e
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2025-09-25 14:00:40 +0200

    Update 006_human_receptor_autoradiography.py

--- examples/milestone_1_1/006_human_receptor_autoradiography.py
+++ examples/milestone_1_1/006_human_receptor_autoradiography.py
@@ -21,55 +21,50 @@ Human receptor autoradiography
 
 # %%
 import siibra
-from nilearn import plotting, image
+from nilearn import plotting
 import matplotlib.pyplot as plt
 
 # sphinx_gallery_thumbnail_path = '_static/example_thumbnails/gaba_autoradiography.png'
 
 # %%
-# Begin by querying for autoradiography volumes registered to MNI 152 2009c
-# Asym Nonl space and select the volume of Gaba/BZ distribution:
+# Autoradiography features contain image data, typically volumes or 2D sections.
+# Here, we query for autoradiography features registered to MNI 152 2009c
+# Asym Nonl space. Autoradiography images are characterized by a receptor. Here,
+# we select the first volume of Gaba/BZ distribution:
 mni152 = siibra.spaces["mni152"]
-autoradiography_images = siibra.features.get(
-    mni152, siibra.features.molecular.AutoradiographyVolumeOfInterest
-)
+autoradiography_images = siibra.features.get(mni152, "autoradiography")
 for f in autoradiography_images:
-    print(f.name)
-    if "GABA/BZ" in f.name:
+    print(f.modality)
+    if "GABA/BZ" in f.modality:
         gaba_autoradiography = f
+        break
 
 # %%
-# We can then plot it over the template, however, the volume has a higher
-# resolution. So we fetch in 600um resolution and upsample the template from 1mm
-# to display it
-img = gaba_autoradiography.fetch(resolution_mm=0.6)
-upsampled_template_img = image.resample_to_img(mni152.get_template().fetch(), img)
-plotting.view_img(
+# Image data can be fetched the online resource and can be plotted on top of the
+# MNI 152 2009c Nonl Asym template. Plot reveals the image covers the complete
+# right hemisphere.
+img = gaba_autoradiography.fetch()
+plotting.plot_stat_map(
     img,
-    bg_img=upsampled_template_img,
     cmap="magma",
-    symmetric_cmap=False,
-    vmin=1e-7,
+    title=gaba_autoradiography.name,
+    threshold=0,
+    vmin=0,
     vmax=7500,
 )
 
 # %%
-# We can make use of siibra to directly draw samples from selected parts of the
-# volume. For this example, draw samples from boxes surrounding regions
-# "4p right", "hoc1 right", and "3b right". Then, compare the distributions
-# using matplotlib:
+# siibra can use the image data to draw samples from spefic regions of interest.
+# Here, we compare GABA/BZ densities in 4p right, hoc1 right, and 3b right.
 julich_brain = siibra.parcellations["julich 3.1"]
 regions = [
-    julich_brain.get_region(spec) for spec in ["4p right", "hoc1 right", "3b right"]
+    julich_brain.get_region(spec)
+    for spec in ["4p right", "hoc1 right", "3b right"]
 ]
-fig, axs = plt.subplots(1, 3, figsize=(10, 5), sharey=True)
+fig, axs = plt.subplots(1, 3, figsize=(10, 5), sharey=True, sharex=True)
 for i, r in enumerate(regions):
-    samples = gaba_autoradiography.draw_samples(
-        25000,
-        voi=r.get_boundingbox(mni152),
-        max_bytes=1 * 1024**3,
-    )
-    sample_values = gaba_autoradiography.evaluate_points(samples, max_bytes=1 * 1024**3)
-    axs[i].hist(sample_values, bins=100)
+    pmap = r.get_regional_map(mni152, "statistical")
+    samples = pmap.draw_samples(10000)
+    values = gaba_autoradiography.evaluate_points(samples)
+    axs[i].hist(values[values > 0], bins=50, density=True)
     axs[i].set_title(r.name)
-    axs[i].set_xlim((1e-7, 7500))
