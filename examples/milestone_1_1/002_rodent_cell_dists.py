c89e74d9840a82a5be79b39f625a8b8341c4c62b
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2025-10-07 14:34:18 +0200

    Update 002_rodent_cell_dists.py

--- examples/milestone_1_1/002_rodent_cell_dists.py
+++ examples/milestone_1_1/002_rodent_cell_dists.py
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 # %%
 # In order to see all available cell distribution features for mice, query with
 # the whole mouse parcellation and display dataset information:
-allen_mice_parc = siibra.parcellations["Allen Mouse Common Coordinate Framework v3 2017"]
+allen_mice_parc = siibra.parcellations["Allen Mouse v3 2017"]
 mice_cell_dists = siibra.features.get(allen_mice_parc, "cell distribution")
 print("Found:", len(mice_cell_dists), "cell distributions")
 datasets = {
@@ -44,36 +44,42 @@ for cd in mice_cell_dists:
     print(f"Region: {cd.anchor}, subject specification: {cd.subject}")
 
 # %%
-# We get the cell distributions for "main olfactory bulb" and overaly that over
-# their masks
-main_olfactory_bulb = allen_mice_parc.get_region("main olfactory bulb")
-main_olfactory_bulb_mask = main_olfactory_bulb.get_regional_mask('allen').fetch()
-mice_olfactory_bulb_dists = [
-    cd
-    for cd in siibra.features.get(main_olfactory_bulb, "cell distribution")
-    if "-" not in cd.subject  # filter out specialized subselections
-]
+# There are several smaller olfactory areas in mice brain, however, we can get
+# cell distributions anchored in them by just querying for the lower level
+# structure "Olfactory areas:
+olfactory_areas = allen_mice_parc.get_region("Olfactory areas")
+olfactory_areas.render_tree()
+olfactory_areas_dists = siibra.features.get(olfactory_areas, "cell distribution")
+
+# %%
+# To display them, we first need the combined mask of all the sub-structures.
+# This can be achieved with `volume.merge` method.
+olfactory_areas_mask = siibra.volumes.volume.merge(
+    [r.get_regional_mask("allen") for r in olfactory_areas]
+).fetch()
+
+# %%
+# We can use the fact that there are several subjects in this dataset by
+# plotting the cell distribution per subject. Furthermore, the cells can be
+# easily colored based on their original anchor.
 allen_mouse_template = siibra.get_template("mouse").fetch()
-fig, axs = plt.subplots(len(mice_olfactory_bulb_dists), 1, figsize=(15, 24))
-for i, cd in enumerate(mice_olfactory_bulb_dists):
-    display = plotting.plot_img(
-        img=allen_mouse_template,
-        bg_img=None,
-        cmap="gray",
-        title=f"{cd.name} - {cd.anchor._regionspec}",
-        cut_coords=cd.data.mean(axis=0),
-        axes=axs[i],
+for subject in {s.subject for s in olfactory_areas_dists}:
+    celldists_for_subject = [cd for cd in olfactory_areas_dists if cd.subject == subject]
+    display = plotting.plot_roi(
+        olfactory_areas_mask,
+        bg_img=allen_mouse_template,
+        colorbar=False,
         draw_cross=False,
-        black_bg=True,
+        title=cd.subject,
+        cut_coords=celldists_for_subject[0].data.mean()
     )
-    display.add_overlay(main_olfactory_bulb_mask, cmap="black_blue_r")
-    display.add_markers(cd.data, marker_color="r", marker_size=1)
+    cmap = plt.get_cmap("tab10", len(celldists_for_subject))
+    for i, cd in enumerate(celldists_for_subject):
+        display.add_markers(cd.data, marker_color=cmap(i), marker_size=1)
 
 # %%
 # In exact same way, the cell distributions for rat brain can be queried.
-# Note that, at the moment one dataset investigating cell distributions is
-# available, and the features differ in region and subjects:
-waxholm_rat_parc = siibra.parcellations["Waxholm Space atlas of the Sprague Dawley rat brain (v4)"]
+waxholm_rat_parc = siibra.parcellations["Waxholm Sprague Dawley rat brain (v4)"]
 rat_cell_dists = siibra.features.get(waxholm_rat_parc, "cell distribution")
 print("Found:", len(rat_cell_dists), "cell distributions")
 datasets = {
@@ -88,26 +94,22 @@ for cd in rat_cell_dists:
     print(f"Region: {cd.anchor}, subject specification: {cd.subject}")
 
 # %%
-# Similarly, display the cells in olfactory bulb for rat
+# Similarly, we can display the parvalbumin positive neurons in olfactory bulb
+# for rat per subject
 olfactory_bulb = waxholm_rat_parc.get_region("olfactory bulb")
 olfactory_bulb_mask = olfactory_bulb.get_regional_mask("waxholm").fetch()
-rat_olfactory_bulb_dists = [
-    cd
-    for cd in siibra.features.get(olfactory_bulb, "cell distribution")
-    if "-" not in cd.subject  # filter out specialized subselections
-]
+rat_olfactory_bulb_dists = siibra.features.get(olfactory_bulb, "cell distribution")
 wax_rat_template = siibra.get_template("Waxholm").fetch()
-fig, axs = plt.subplots(len(rat_olfactory_bulb_dists), 1, figsize=(15, 24))
-for i, cd in enumerate(rat_olfactory_bulb_dists):
-    display = plotting.plot_img(
-        img=wax_rat_template,
-        bg_img=None,
-        cmap="gray",
-        title=f"{cd.name} - {cd.anchor._regionspec}",
-        cut_coords=cd.data.mean(axis=0),
-        axes=axs[i],
+for subject in {s.subject for s in rat_olfactory_bulb_dists}:
+    celldists_for_subject = [cd for cd in rat_olfactory_bulb_dists if cd.subject == subject]
+    display = plotting.plot_roi(
+        olfactory_bulb_mask,
+        bg_img=wax_rat_template,
+        colorbar=False,
         draw_cross=False,
-        black_bg=True,
+        title=cd.subject,
+        cut_coords=celldists_for_subject[0].data.mean()
     )
-    display.add_overlay(olfactory_bulb_mask, cmap="black_blue_r")
-    display.add_markers(cd.data, marker_color="r", marker_size=1)
+    cmap = plt.get_cmap("jet", len(celldists_for_subject))
+    for i, cd in enumerate(celldists_for_subject):
+        display.add_markers(cd.data, marker_color=cmap(i), marker_size=1)
