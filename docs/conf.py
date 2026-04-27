ce72e3c26ed2c2d0965ebc636ae8371b31bed9ae
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2026-04-27 15:13:58 +0200

    remove examples to speed up doc creation for testing the paper notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -167,19 +167,9 @@ autosummary_generate = True
 # example gallery details
 sphinx_gallery_conf = {
     "examples_dirs": [
-        "../examples/01_atlases_and_parcellations",
-        "../examples/02_maps_and_templates",
-        "../examples/03_data_features",
-        "../examples/04_locations",
-        "../examples/05_anatomical_assignment",
         "../examples/tutorials",
     ],
     "gallery_dirs": [
-        "examples/01_atlases_and_parcellations",
-        "examples/02_maps_and_templates",
-        "examples/03_data_features",
-        "examples/04_locations",
-        "examples/05_anatomical_assignment",
         "examples/tutorials",
     ],
     "filename_pattern": r"^.*.py",  # which files to execute and include their outputs
